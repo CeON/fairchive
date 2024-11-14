b899f28eb1d81c65f1f92f6387ade44807bd513f
lbownik <lbownik@icm.edu.pl>
2024-11-14 13:05:43 +0100

    removed empty lines

--- dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/ingest/IngestableDataCheckerTest.java
+++ dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/ingest/IngestableDataCheckerTest.java
@@ -15,7 +15,6 @@ public class IngestableDataCheckerTest {
 
     private ByteBuffer createBufferContaining(final String fileContents)
             throws IOException {
-
         final ByteBuffer buffer = ByteBuffer.allocate(100);
         buffer.put(fileContents.getBytes());
         buffer.rewind();
@@ -25,7 +24,6 @@ public class IngestableDataCheckerTest {
 
     private AbstractStringAssert<?> assertThatADATA(final String content)
             throws Exception {
-
         ByteBuffer buff = createBufferContaining(content);
 
         return assertThat(this.instance.testDTAformat(buff));
@@ -33,7 +31,6 @@ public class IngestableDataCheckerTest {
 
     private AbstractStringAssert<?> assertThatSAV(final String content)
             throws Exception {
-
         ByteBuffer buff = createBufferContaining(content);
 
         return assertThat(this.instance.testSAVformat(buff));
@@ -41,27 +38,23 @@ public class IngestableDataCheckerTest {
 
     @Test
     public void testingADATA_returnsMimeType_forProperContent() throws Exception {
-
         assertThatADATA("l   ").isEqualTo("application/x-stata");
         assertThatADATA(STATA_13_HEADER).isEqualTo("application/x-stata-13");
     }
 
     @Test
     public void testingADATA_returnsNull_forBrokenContent() throws Exception {
-
         assertThatADATA("").isNull();
         assertThatADATA("hello-non-stata-file-how-are-you").isNull();
     }
 
     @Test
     public void testingSAV_returnsMimeType_forProperContent() throws Exception {
-
         assertThatSAV("$FL2").isEqualTo("application/x-spss-sav");
     }
 
     @Test
     public void testingSAV_returnsNull_forBrokenContent() throws Exception {
-
         assertThatSAV("").isNull();
         assertThatSAV("i-am-not-a-x-spss-sav-file").isNull();
     }
