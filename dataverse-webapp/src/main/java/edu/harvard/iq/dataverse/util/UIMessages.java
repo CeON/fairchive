7c1f60276c4a6aad4e3c060f47b0d82c03a4955a
lbownik <lbownik@icm.edu.pl>
2025-01-31 18:28:40 +0100

    rewritten UIMessages so that it doesn't use JsfHelper

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/util/UIMessages.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/util/UIMessages.java
@@ -1,32 +1,54 @@
 package edu.harvard.iq.dataverse.util;
 
+import static edu.harvard.iq.dataverse.common.BundleUtil.getStringFromBundle;
+import static javax.faces.application.FacesMessage.SEVERITY_ERROR;
+
 import javax.ejb.Stateless;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.PrimeFaces;
+
 @Stateless
 public class UIMessages {
-
+    
     public void addSuccessMessage(final String message) {
-        JsfHelper.addSuccessMessage(message);
+        addComponentSuccessMessage("successMessage",
+                getStringFromBundle("messages.success"), message);
     }
 
     public void addErrorMessage(final String message) {
-        JsfHelper.addErrorMessage(message);
+        addAjaxHasErrorParam();
+        addComponentErrorMessage((String) null,
+                getStringFromBundle("messages.error"), message);
     }
 
     public void addFlashSuccessMessage(final String message) {
-        JsfHelper.addFlashSuccessMessage(message);
+        addAjaxHasErrorParam();
+        addSuccessMessage(message);
+    }
+    
+    private static void addAjaxHasErrorParam() {
+        PrimeFaces.current().ajax().addCallbackParam("hasErrorMessage", true);
+    }
+
+    public void addComponentSuccessMessage(final String componentId,
+            final String summary, final String detail) {
+        FacesContext.getCurrentInstance().addMessage(componentId,
+                new FacesMessage(summary, detail));
     }
 
     public void addComponentErrorMessage(final String componentId,
             final String summary, final String detail) {
-        JsfHelper.addComponentErrorMessage(componentId, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(componentId,
+                new FacesMessage(SEVERITY_ERROR, summary, detail));
     }
+
     public void addComponentErrorMessage(final UIComponent component,
             final String summary, final String detail) {
         final FacesContext context = FacesContext.getCurrentInstance();
-        context.validationFailed();
-        JsfHelper.addComponentErrorMessage(component.getClientId(context), summary, detail);
-    }   
+        final String componentId = component.getClientId(context);
+        addComponentErrorMessage(componentId, summary, detail);
+    }
 }
