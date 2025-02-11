b525907be5d1957c53bc51f89c651cc903b974ad
lbownik <lukasz.bownik@gmail.com>
2025-02-11 10:33:01 +0100

    Closes #2649 Provides preview for obj,3ds,fbx,stl files (#2655)
    * Closes #2649 Provides preview for obj,3ds,fbx,stl files
    
    
    ---------
    
    Co-authored-by: lbownik <lbownik@icm.edu.pl>

--- dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/datafile/ExternalToolRepository.java
+++ dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/datafile/ExternalToolRepository.java
@@ -1,38 +1,13 @@
 package edu.harvard.iq.dataverse.persistence.datafile;
 
-import java.util.List;
-
 import javax.ejb.Stateless;
 
 import edu.harvard.iq.dataverse.persistence.JpaRepository;
-import edu.harvard.iq.dataverse.persistence.datafile.ExternalTool.Type;
 
 @Stateless
 public class ExternalToolRepository extends JpaRepository<Long, ExternalTool> {
 
-    // -------------------- CONSTRUCTORS --------------------
-
     public ExternalToolRepository() {
         super(ExternalTool.class);
-    }
-
-    // -------------------- LOGIC --------------------
-
-    public List<ExternalTool> findByType(final Type type, final String contentType) {
-        return this.em.createQuery(
-                "SELECT OBJECT(o) FROM ExternalTool AS o " +
-                        "WHERE o.type = :type AND o.contentType = :contentType",
-                ExternalTool.class)
-                .setParameter("type", type)
-                .setParameter("contentType", contentType)
-                .getResultList();
-    }
-
-    public List<ExternalTool> findByType(final Type type) {
-        return this.em.createQuery(
-                "SELECT OBJECT(o) FROM ExternalTool AS o WHERE o.type = :type",
-                ExternalTool.class)
-                .setParameter("type", type)
-                .getResultList();
     }
 }
