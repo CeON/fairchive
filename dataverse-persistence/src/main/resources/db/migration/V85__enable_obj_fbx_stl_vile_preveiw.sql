e0d266173c7c6aa60f15562ef3258bbf8bfad0cd
lbownik <lbownik@icm.edu.pl>
2025-02-13 09:13:38 +0100

    fixed broken migration

--- dataverse-persistence/src/main/resources/db/migration/V85__enable_obj_fbx_stl_vile_preveiw.sql
+++ dataverse-persistence/src/main/resources/db/migration/V85__enable_obj_fbx_stl_vile_preveiw.sql
@@ -1,3 +1,5 @@
+ALTER TABLE externaltool ADD COLUMN fileextention text;
+
 INSERT INTO public.externaltool (description,displayname,toolparameters,toolurl,"type",contenttype,fileextention) VALUES
      ('Preview 3D model','Preview 3D model','{"queryParameters":[{"fileUrl64":"{fileUrl64}"}]}','{siteUrl}/3Dviewer.xhtml','PREVIEW','text/plain','obj'),
      ('Preview 3D model','Preview 3D model','{"queryParameters":[{"fileUrl64":"{fileUrl64}"}]}','{siteUrl}/3Dviewer.xhtml','PREVIEW','application/octet-stream','fbx'),
