b8e9f4bb96a411ed9d7e575685855b7ab7ae7a4b
Krzysztof Mądry <k.madry@icm.edu.pl>
2025-02-18 09:42:43 +0100

    Closes #2656: Add validators checking if field is not greater than other field and checking if field is not greater than specific value (#2657)
    Co-authored-by: Sylwester Niewczas <sniewcza@icm.edu.pl>
    Co-authored-by: Filipe Dias Lewandowski <fdias@icm.edu.pl>

---
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/validation/field/validators/NotGreaterThanValidator.java
@@ -0,0 +1,52 @@
+package edu.harvard.iq.dataverse.validation.field.validators;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.commons.lang3.StringUtils;
+import org.omnifaces.cdi.Eager;
+
+import edu.harvard.iq.dataverse.common.BundleUtil;
+import edu.harvard.iq.dataverse.persistence.dataset.ValidatableField;
+import edu.harvard.iq.dataverse.validation.field.FieldValidationResult;
+
+/**
+ * Validator that checks whether field is not greater then dependant field.
+ * 
+ * @author Krzysztof Mądry
+ * @author Sylwester Niewczas
+ */
+@Eager
+@ApplicationScoped
+public class NotGreaterThanValidator extends DependantFieldValidator {
+
+    @Override
+    public String getName() {
+        return "not_greater_than_validator";
+    }
+
+    @Override
+    protected FieldValidationResult validateWithDependantField(ValidatableField field, ValidatableField dependantField,
+            Map<String, Object> params, Map<String, ? extends List<? extends ValidatableField>> fieldIndex) {
+        
+        String value = field.getSingleValue();
+        long valueLong = Long.parseLong(value);
+        
+        String compareToValue = dependantField.getSingleValue();
+        if (StringUtils.isBlank(compareToValue)) {
+            return FieldValidationResult.ok();
+        }
+        long compareToValueLong = Long.parseLong(compareToValue);
+
+        if (valueLong > compareToValueLong) {
+            return FieldValidationResult.invalid(field, BundleUtil.getStringFromBundle("isGreaterThanField",
+                    field.getDatasetFieldType().getDisplayName(),
+                    dependantField.getDatasetFieldType().getDisplayName()));
+        }
+
+        return FieldValidationResult.ok();
+    }
+
+}
