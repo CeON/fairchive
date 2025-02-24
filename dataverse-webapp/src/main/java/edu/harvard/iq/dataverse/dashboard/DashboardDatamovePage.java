48322efbfc5da3b6f8a357ef1406d9c36337735a
lbownik <lukasz.bownik@gmail.com>
2025-02-24 15:02:26 +0100

    2670 suggestion component on move dataset tab should be wider (#2671)
    * Closes #2670 Suggestion component on "Move dataset" tab should be wider
    
    * Introduced constructor injection
    reworked initialization
    removed unused method
    static imports
    
    * introduced UIMessages
    static imports
    accidentally autoformatted everything (sorry)
    
    * removed unnecessary enumeration
    
    * typo fixed
    
    * reset input controls after successful move operation
    
    ---------
    
    Co-authored-by: lbownik <lbownik@icm.edu.pl>

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/dashboard/DashboardDatamovePage.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/dashboard/DashboardDatamovePage.java
@@ -1,17 +1,18 @@
 package edu.harvard.iq.dataverse.dashboard;
 
 import static edu.harvard.iq.dataverse.common.BundleUtil.getStringFromBundle;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.logging.Level.WARNING;
+import static java.util.stream.Collectors.joining;
+import static org.apache.commons.lang.StringUtils.EMPTY;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -22,7 +23,7 @@ import edu.harvard.iq.dataverse.DatasetDao;
 import edu.harvard.iq.dataverse.DataverseRequestServiceBean;
 import edu.harvard.iq.dataverse.DataverseSession;
 import edu.harvard.iq.dataverse.EjbDataverseEngine;
-import edu.harvard.iq.dataverse.PermissionsWrapper;
+import edu.harvard.iq.dataverse.NavigationWrapper;
 import edu.harvard.iq.dataverse.common.BundleUtil;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
@@ -34,52 +35,50 @@ import edu.harvard.iq.dataverse.persistence.GlobalId;
 import edu.harvard.iq.dataverse.persistence.dataset.Dataset;
 import edu.harvard.iq.dataverse.persistence.dataverse.Dataverse;
 import edu.harvard.iq.dataverse.persistence.dataverse.DataverseRepository;
-import edu.harvard.iq.dataverse.persistence.user.User;
 import edu.harvard.iq.dataverse.settings.SettingsWrapper;
-import edu.harvard.iq.dataverse.util.JsfHelper;
-import edu.harvard.iq.dataverse.util.SystemConfig;
+import edu.harvard.iq.dataverse.util.UIMessages;
 
+@SuppressWarnings("serial")
 @ViewScoped
 @Named("DashboardDatamovePage")
 public class DashboardDatamovePage implements Serializable {
-    private static final Logger logger = Logger.getLogger(DashboardDatamovePage.class.getCanonicalName());
+    private static final Logger logger = Logger
+            .getLogger(DashboardDatamovePage.class.getCanonicalName());
 
-    @Inject
-    DataverseRequestServiceBean requestService;
-
-    @EJB
-    private DatasetDao datasetDao;
-
-    @EJB
-    private DataverseRepository dataverseRepo;
-
-    @Inject
-    private DataverseSession session;
-
-    @Inject
-    private PermissionsWrapper permissionsWrapper;
-
-    @EJB
-    private EjbDataverseEngine commandEngine;
-
-    @Inject
-    private SettingsWrapper settings;
-
-    @Inject
-    private SystemConfig systemConfig;
+    private final DataverseRequestServiceBean requestService;
+    private final DatasetDao datasetDao;
+    private final DataverseRepository dataverseRepo;
+    private final DataverseSession session;
+    private final NavigationWrapper navigation;
+    private final EjbDataverseEngine commandEngine;
+    private final SettingsWrapper settings;
+    private final UIMessages uiMessages;
 
     private boolean forceMove = false;
 
-    // Following two are for dataset move
-    private List<Dataset> sourceDatasets;
-
+    private List<Dataset> sourceDatasets = new ArrayList<>();
     private Dataverse targetDataverse;
-
-    // And following two for dataverse move
 
     private Dataverse sourceDataverse;
 
-    // -------------------- GETTERS --------------------
+    @Inject
+    public DashboardDatamovePage(final DataverseRequestServiceBean requestService,
+            final DatasetDao datasetDao,
+            final DataverseRepository dataverseRepo,
+            final DataverseSession session,
+            final NavigationWrapper navigation,
+            final EjbDataverseEngine commandEngine,
+            final SettingsWrapper settings,
+            final UIMessages uiMessages) {
+        this.requestService = requestService;
+        this.datasetDao = datasetDao;
+        this.dataverseRepo = dataverseRepo;
+        this.session = session;
+        this.navigation = navigation;
+        this.commandEngine = commandEngine;
+        this.settings = settings;
+        this.uiMessages = uiMessages;
+    }
 
     public boolean isForceMove() {
         return forceMove;
@@ -98,102 +97,116 @@ public class DashboardDatamovePage implements Serializable {
     }
 
     // -------------------- LOGIC --------------------
-
-    public String init() {
-        User user = session.getUser();
-        if (!user.isSuperuser() || systemConfig.isReadonlyMode()) {
-            return permissionsWrapper.notAuthorized();
-        }
-        sourceDatasets = new ArrayList<>();
-        return StringUtils.EMPTY;
+    public String verifyAccess() {
+        return this.session.canEditDashboard() 
+                ? EMPTY
+                : this.navigation.notAuthorized();
     }
 
-    public List<Dataset> completeSourceDataset(String query) {
+    public List<Dataset> completeSourceDataset(final String query) {
         if (query.contains("/")) {
-            Dataset ds = datasetDao.findByGlobalId(query);
-            return ds != null ? Collections.singletonList(ds) : Collections.emptyList();
+            final Dataset ds = datasetDao.findByGlobalId(query);
+            return ds != null ? singletonList(ds) : emptyList();
+        } else {
+            return emptyList();
         }
-        return Collections.emptyList();
     }
 
     public List<Dataverse> completeDataverse(final String query) {
-        return this.dataverseRepo.findByAliasOrNameOrAffiliation(query, query, query);
+        return this.dataverseRepo.findByAliasOrNameOrAffiliation(query, query,
+                query);
     }
 
     public void moveDataset() {
-        if (sourceDatasets == null || sourceDatasets.isEmpty() || targetDataverse == null) {
-            // We should never get here, but in case of some unexpected failure we should be prepared nevertheless
-            JsfHelper.addErrorMessage(getStringFromBundle("dashboard.datamove.empty.fields"));
+        if (sourceDatasets == null || sourceDatasets.isEmpty()
+                || targetDataverse == null) {
+            // We should never get here, but in case of some unexpected failure we
+            // should be prepared nevertheless
+            this.uiMessages.addErrorMessage(
+                    getStringFromBundle("dashboard.datamove.empty.fields"));
             return;
         }
 
         List<String> successfulIds = new ArrayList<>();
         List<String> failureMessages = new ArrayList<>();
         for (Dataset source : sourceDatasets) {
-            Summary summary = new Summary(Summary.Mode.DATASET);
+            Summary summary = new Summary("dashboard.datamove.dataset", this.uiMessages);
             try {
                 summary.addParameter(source.getDisplayName())
                         .addParameter(extractSourcePersistentId(source))
                         .addParameter(targetDataverse.getDisplayName());
 
-                DataverseRequest dataverseRequest = requestService.getDataverseRequest();
+                DataverseRequest dataverseRequest = requestService
+                        .getDataverseRequest();
                 String previousSourceAlias = extractSourceAlias(source);
-                commandEngine.submit(new MoveDatasetCommand(dataverseRequest, source, targetDataverse, forceMove));
-                logger.info(createMessageWithDatasetMoveInfo(source, "Moved", previousSourceAlias));
+                commandEngine.submit(new MoveDatasetCommand(dataverseRequest, source,
+                        targetDataverse, forceMove));
+                logger.info(createMessageWithDatasetMoveInfo(source, "Moved",
+                        previousSourceAlias));
                 successfulIds.add(extractSourcePersistentId(source));
             } catch (MoveException me) {
-                logger.log(Level.WARNING, createMessageWithDatasetMoveInfo(source, "Unable to move"), me);
-                summary.addParameter(me).addParameter(createForceInfoIfApplicable(me));
+                logger.log(WARNING, createMessageWithDatasetMoveInfo(source, "Unable to move"), me);
+                summary.addParameter(me)
+                        .addParameter(createForceInfoIfApplicable(me));
                 failureMessages.add(summary.getFailureMessageDetail());
             } catch (CommandException ce) {
-                logger.log(Level.WARNING, createMessageWithDatasetMoveInfo(source, "Unable to move"), ce);
-                summary.addParameter(getStringFromBundle("dashboard.datamove.dataset.message.failure.summary"));
+                logger.log(WARNING, createMessageWithDatasetMoveInfo(source, "Unable to move"), ce);
+                summary.addParameter(getStringFromBundle(
+                        "dashboard.datamove.dataset.message.failure.summary"));
                 failureMessages.add(summary.getFailureMessageDetail());
             }
         }
 
-        showDatasetsMovedMessage(successfulIds, failureMessages, targetDataverse.getDisplayName());
+        resetDatasetMoveFields();
+        showDatasetsMovedMessage(successfulIds, failureMessages,
+                targetDataverse.getDisplayName());
     }
 
     public void moveDataverse() {
         if (sourceDataverse == null || targetDataverse == null) {
-            JsfHelper.addErrorMessage(getStringFromBundle("dashboard.datamove.empty.fields"));
+            this.uiMessages.addErrorMessage(
+                    getStringFromBundle("dashboard.datamove.empty.fields"));
             return;
         }
 
-        Summary summary = new Summary(Summary.Mode.DATAVERSE)
+        Summary summary = new Summary("dashboard.datamove.dataverse", this.uiMessages)
                 .addParameter(extractDataverseAlias(sourceDataverse))
                 .addParameter(extractDataverseAlias(targetDataverse));
 
         try {
             DataverseRequest dataverseRequest = requestService.getDataverseRequest();
-            commandEngine.submit(new MoveDataverseCommand(dataverseRequest, sourceDataverse, targetDataverse, forceMove));
+            commandEngine.submit(new MoveDataverseCommand(dataverseRequest,
+                    sourceDataverse, targetDataverse, forceMove));
             logger.info(createMessageWithDataverseMoveInfo("Moved"));
             resetDataverseMoveFields();
             summary.showSuccessMessage();
         } catch (MoveException me) {
-            logger.log(Level.WARNING, createMessageWithDataverseMoveInfo("Unable to move"), me);
+            logger.log(WARNING, createMessageWithDataverseMoveInfo("Unable to move"), me);
             summary.addParameter(me)
                     .addParameter(createForceInfoIfApplicable(me))
                     .showFailureMessage();
         } catch (CommandException ce) {
-            logger.log(Level.WARNING, createMessageWithDataverseMoveInfo("Unable to move"), ce);
-            JsfHelper.addErrorMessage(getStringFromBundle("dashboard.datamove.dataverse.message.failure.summary"), StringUtils.EMPTY);
+            logger.log(WARNING, createMessageWithDataverseMoveInfo("Unable to move"), ce);
+            this.uiMessages.addErrorMessage(
+                    getStringFromBundle("dashboard.datamove.dataverse.message.failure.summary"), EMPTY);
         }
     }
 
     public String getMessageDetails() {
-        return getStringFromBundle("dashboard.datamove.message.details", settings.getGuidesBaseUrl(), settings.getGuidesVersion());
+        return getStringFromBundle("dashboard.datamove.message.details",
+                settings.getGuidesBaseUrl(), settings.getGuidesVersion());
     }
 
     // -------------------- PRIVATE --------------------
 
-    private static void showDatasetsMovedMessage(List<String> successfulIds, List<String> failureMessages, String dataverseName) {
+    private void showDatasetsMovedMessage(List<String> successfulIds,
+            List<String> failureMessages, String dataverseName) {
         StringBuilder sb = new StringBuilder();
 
         if (!successfulIds.isEmpty()) {
-            sb.append(getStringFromBundle("dashboard.datamove.dataset.message.success.multiple",
-                            StringUtils.join(successfulIds, ", "), dataverseName));
+            sb.append(getStringFromBundle(
+                    "dashboard.datamove.dataset.message.success.multiple",
+                    StringUtils.join(successfulIds, ", "), dataverseName));
         }
 
         if (!failureMessages.isEmpty()) {
@@ -203,144 +216,128 @@ public class DashboardDatamovePage implements Serializable {
             sb.append(String.join("<hr>", failureMessages));
         }
 
-        JsfHelper.addInfoMessage(sb.toString().replaceAll("<br><br>", "<br>"));
+        this.uiMessages.addInfoMessage(sb.toString().replaceAll("<br><br>", "<br>"));
     }
 
     private static String extractSourcePersistentId(Dataset source) {
         return Optional.ofNullable(source)
                 .map(Dataset::getGlobalId)
                 .map(GlobalId::asString)
-                .orElse(StringUtils.EMPTY);
+                .orElse(EMPTY);
     }
 
     private static String extractSourceAlias(Dataset source) {
         return Optional.ofNullable(source)
                 .map(Dataset::getOwner)
                 .map(Dataverse::getAlias)
-                .orElse(StringUtils.EMPTY);
+                .orElse(EMPTY);
     }
 
     private static String extractDataverseAlias(Dataverse dataverse) {
         return Optional.ofNullable(dataverse)
                 .map(Dataverse::getAlias)
-                .orElse(StringUtils.EMPTY);
+                .orElse(EMPTY);
     }
 
-    private String createMessageWithDatasetMoveInfo(Dataset sourceDs, String message, String source) {
+    private String createMessageWithDatasetMoveInfo(Dataset sourceDs, String message,
+            String source) {
         return String.format("%s %s from %s to %s",
-                message, extractSourcePersistentId(sourceDs), source, extractDataverseAlias(targetDataverse));
+                message, extractSourcePersistentId(sourceDs), source,
+                extractDataverseAlias(targetDataverse));
     }
 
-    private String createMessageWithDatasetMoveInfo(Dataset sourceDs, String message) {
-        return createMessageWithDatasetMoveInfo(sourceDs, message, extractSourceAlias(sourceDs));
+    private String createMessageWithDatasetMoveInfo(Dataset sourceDs,
+            String message) {
+        return createMessageWithDatasetMoveInfo(sourceDs, message,
+                extractSourceAlias(sourceDs));
     }
 
     private String createMessageWithDataverseMoveInfo(String message) {
         return String.format("%s %s to %s",
-                message, extractDataverseAlias(sourceDataverse), extractDataverseAlias(targetDataverse));
+                message, extractDataverseAlias(sourceDataverse),
+                extractDataverseAlias(targetDataverse));
     }
 
     private String createForceInfoIfApplicable(MoveException mde) {
         return isForcingPossible(mde)
                 ? getStringFromBundle("dashboard.datamove.command.suggestForce",
-                settings.getGuidesBaseUrl(), settings.getGuidesVersion())
-                : StringUtils.EMPTY;
+                        settings.getGuidesBaseUrl(), settings.getGuidesVersion())
+                : EMPTY;
     }
 
     private boolean isForcingPossible(MoveException mde) {
         return mde.getDetails().stream()
                 .allMatch(AdditionalMoveStatus::isPassByForcePossible);
     }
-
+    
     private void resetDatasetMoveFields() {
-        sourceDatasets = new ArrayList<>();
-        targetDataverse = null;
+        this.sourceDatasets = new ArrayList<>();
+        this.targetDataverse = null;
     }
 
     private void resetDataverseMoveFields() {
-        sourceDataverse = null;
-        targetDataverse = null;
+        this.sourceDataverse = null;
+        this.targetDataverse = null;
     }
 
     // -------------------- SETTERS --------------------
 
-    public void setForceMove(boolean forceMove) {
+    public void setForceMove(final boolean forceMove) {
         this.forceMove = forceMove;
     }
 
-    public void setSourceDatasets(List<Dataset> sourceDatasets) {
+    public void setSourceDatasets(final List<Dataset> sourceDatasets) {
         this.sourceDatasets = sourceDatasets;
     }
 
-    public void setTargetDataverse(Dataverse targetDataverse) {
+    public void setTargetDataverse(final Dataverse targetDataverse) {
         this.targetDataverse = targetDataverse;
     }
 
-    public void setSourceDataverse(Dataverse sourceDataverse) {
+    public void setSourceDataverse(final Dataverse sourceDataverse) {
         this.sourceDataverse = sourceDataverse;
     }
 
     // -------------------- INNER CLASSES ---------------------
 
     private static class Summary {
-        public enum Mode {
-            DATAVERSE("dashboard.datamove.dataverse"),
-            DATASET("dashboard.datamove.dataset");
 
-            private String key;
-
-            Mode(String key) {
-                this.key = key;
-            }
-
-            public String getKey() {
-                return key;
-            }
-        }
-
-        private Mode mode;
-
+        private final String key;
         private final List<String> summaryParameters = new ArrayList<>();
+        private final UIMessages uiMessages;
 
-        // -------------------- CONSTRUCTORS --------------------
-
-        public Summary(Mode mode) {
-            this.mode = mode;
+        public Summary(final String key, final UIMessages uiMessages) {
+            this.key = key;
+            this.uiMessages = uiMessages;
         }
 
-        // -------------------- LOGIC --------------------
-
-        public Summary addParameter(String param) {
-            summaryParameters.add(param != null ? param : StringUtils.EMPTY);
+        public Summary addParameter(final String param) {
+            this.summaryParameters.add(param != null ? param : EMPTY);
             return this;
         }
 
-        public Summary addParameter(MoveException mde) {
-            summaryParameters.add(mde.getDetails().stream()
+        public Summary addParameter(final MoveException mde) {
+            this.summaryParameters.add(mde.getDetails().stream()
                     .map(AdditionalMoveStatus::getMessageKey)
                     .map(BundleUtil::getStringFromBundle)
-                    .collect(Collectors.joining(" ")));
+                    .collect(joining(" ")));
             return this;
         }
 
         public void showSuccessMessage() {
-            JsfHelper.addFlashSuccessMessage(getStringFromBundle(buildKey("message.success"), summaryParameters.toArray()));
+            this.uiMessages.addFlashSuccessMessage(getStringFromBundle(
+                    this.key.concat(".message.success"), this.summaryParameters.toArray()));
         }
 
         public String getFailureMessageDetail() {
-            return getStringFromBundle(buildKey("message.failure.details"), summaryParameters.toArray());
+            return getStringFromBundle(this.key.concat(".message.failure.details"),
+                    this.summaryParameters.toArray());
         }
 
         public void showFailureMessage() {
-            JsfHelper.addErrorMessage(
-                    getStringFromBundle(buildKey("message.failure.summary")),
+            this.uiMessages.addErrorMessage(
+                    getStringFromBundle(this.key.concat(".message.failure.summary")),
                     getFailureMessageDetail());
-        }
-
-        // -------------------- PRIVATE --------------------
-
-        private String buildKey(String postfix) {
-            return mode.getKey() + '.' + postfix;
         }
     }
 }
--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/util/UIMessages.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/util/UIMessages.java
@@ -2,6 +2,7 @@ package edu.harvard.iq.dataverse.util;
 
 import static edu.harvard.iq.dataverse.common.BundleUtil.getStringFromBundle;
 import static javax.faces.application.FacesMessage.SEVERITY_ERROR;
+import static javax.faces.application.FacesMessage.SEVERITY_INFO;
 
 import javax.ejb.Stateless;
 import javax.faces.application.FacesMessage;
@@ -22,6 +23,15 @@ public class UIMessages {
         addAjaxHasErrorParam();
         addComponentErrorMessage((String) null, message);
     }
+    
+    public void addErrorMessage(final String message, final String detail) {
+        addAjaxHasErrorParam();
+        addComponentErrorMessage((String) null, message, detail);
+    }
+    
+    public void addInfoMessage(final String detail) {
+        addComponentInfoMessage(null, getStringFromBundle("messages.info"), detail);
+    }
 
     public void addFlashSuccessMessage(final String message) {
         addAjaxHasErrorParam();
@@ -36,6 +46,12 @@ public class UIMessages {
             final String summary, final String detail) {
         FacesContext.getCurrentInstance().addMessage(componentId,
                 new FacesMessage(summary, detail));
+    }
+    
+    public void addComponentInfoMessage(final String componentId,
+            final String message, final String detail) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(SEVERITY_INFO, message, detail));
     }
 
     public void addComponentErrorMessage(final String componentId,
