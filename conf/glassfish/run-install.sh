706b7b831e07bef89e96d488349ad5595c4db55a
Filipe Dias Lewandowski <diasf@users.noreply.github.com>
2025-02-28 15:30:09 +0100

    Refs CeON/mxrdr-extension#124: Small improvements to dev script to allow extensions to have additional installation steps (#2685)

--- conf/glassfish/run-install.sh
+++ conf/glassfish/run-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DATAVERSE_CONTAINER_WORKING_DIR="/dataverse/dataverse-dist/target/container"
+export DATAVERSE_CONTAINER_WORKING_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "* Copying properties"
 mkdir /root/.dataverse
-cp ${DATAVERSE_CONTAINER_WORKING_DIR}/dataverse.properties /root/.dataverse/
+cp "${DATAVERSE_CONTAINER_WORKING_DIR}/dataverse.properties" /root/.dataverse/
 
 echo "* Copying default config"
-cp ${DATAVERSE_CONTAINER_WORKING_DIR}/default.config /dataverse/scripts/installer/
+cp "${DATAVERSE_CONTAINER_WORKING_DIR}/default.config" /dataverse/scripts/installer/
 
 echo "* Configuring prometheus metrics export"
-cp ${DATAVERSE_CONTAINER_WORKING_DIR}/prometheus_exporter.yaml /opt/prometheus_javagent
+cp "${DATAVERSE_CONTAINER_WORKING_DIR}/prometheus_exporter.yaml" /opt/prometheus_javagent
 export GLASSFISH_JAVAAGENT="/opt/prometheus_javagent/jmx_prometheus_javaagent-1.1.0.jar=9999\:/opt/prometheus_javagent/prometheus_exporter.yaml"
 
 export WARFILE_LOCATION="${DATAVERSE_CONTAINER_WORKING_DIR}/dataverse.war"
