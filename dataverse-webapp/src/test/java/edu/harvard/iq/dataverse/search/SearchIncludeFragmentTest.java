51e077c761351f8359813accc1c55538c648e8ab
lbownik <lbownik@icm.edu.pl>
2025-03-10 10:21:51 +0100

    test fixed

--- dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/search/SearchIncludeFragmentTest.java
+++ dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/search/SearchIncludeFragmentTest.java
@@ -9,13 +9,14 @@ import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,6 +26,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.primefaces.component.tabview.Tab;
 import org.primefaces.event.TabChangeEvent;
 import org.primefaces.model.StreamedContent;
@@ -38,11 +41,14 @@ import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldType;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldTypeRepository;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetRepository;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion;
+import edu.harvard.iq.dataverse.persistence.dataset.MetadataBlock;
+import edu.harvard.iq.dataverse.persistence.dataset.MetadataBlockRepository;
 import edu.harvard.iq.dataverse.persistence.dataverse.Dataverse;
 import edu.harvard.iq.dataverse.search.response.SolrQueryResponse;
 import edu.harvard.iq.dataverse.search.response.SolrSearchResult;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class SearchIncludeFragmentTest {
 
     private SearchIncludeFragment fragment = new SearchIncludeFragment();
@@ -56,11 +62,14 @@ public class SearchIncludeFragmentTest {
     private DatasetFieldTypeRepository datasetFieldTypeRepo;
     @Mock
     private DatasetRepository datasetRepo;
+    @Mock
+    MetadataBlockRepository metadataBlockRepo;
 
     private Dataverse dataverse = new Dataverse();
 
     private DatasetFieldType type1 = new DatasetFieldType();
     private DatasetFieldType type2 = new DatasetFieldType();
+    private MetadataBlock metaBlock = new MetadataBlock();
 
     private Dataset dataset = new Dataset();
 
@@ -75,6 +84,7 @@ public class SearchIncludeFragmentTest {
         this.fragment.lastSearchValue = new LastSearchValue();
         this.fragment.datasetFieldTypeRepo = this.datasetFieldTypeRepo;
         this.fragment.datasetRepo = this.datasetRepo;
+        this.fragment.metadataBlockRepo = metadataBlockRepo;
         this.fragment.postConstruct();
 
         this.dataverse.setId(1L);
@@ -85,6 +95,11 @@ public class SearchIncludeFragmentTest {
         this.type1.setExportToFile(true);
         this.type2.setId(2L);
         this.type2.setTitle("abc");
+        
+        this.metaBlock.setName("Block1");
+        this.metaBlock.setDatasetFieldTypes(asList(this.type1, this.type2));
+        this.type1.setMetadataBlock(this.metaBlock);
+        this.type2.setMetadataBlock(this.metaBlock);
 
         DatasetVersion dsv = new DatasetVersion();
         DatasetField df = new DatasetField();
@@ -113,14 +128,17 @@ public class SearchIncludeFragmentTest {
         intendedResult.setEntityId(1L);
         intendedResult.setName("name1");
         intendedResult.setTitle("title1");
+        
         when(this.searchService.search(any(), any(), anyString(), any(), any(), any(),
-                any(), anyInt(), anyInt(), anyBoolean()))
-                .thenReturn(responseOf(intendedResult));
-        when(this.datasetRepo.getById(eq(intendedResult.getEntityId())))
-                .thenReturn(this.dataset);
+                any(), anyInt(), anyInt(), anyBoolean())).thenReturn(responseOf());
+        when(this.searchService.search(any(SolrQuery.class))).thenReturn(asList(intendedResult)); 
+        when(this.datasetRepo.findById(eq(intendedResult.getEntityId())))
+                .thenReturn(Optional.of(this.dataset));
         when(this.datasetFieldTypeRepo.findAll())
                 .thenReturn(asList(this.type1, this.type2));
-
+        when(this.metadataBlockRepo.findSystemMetadataBlocks())
+            .thenReturn(asList(this.metaBlock));
+        
         this.fragment.search();
         StreamedContent file = this.fragment.getSearchResultsFile();
 
@@ -131,7 +149,7 @@ public class SearchIncludeFragmentTest {
 
         List<String> lines = readLines(file.getStream(), "utf-8");
         assertThat(lines.size()).isEqualTo(2);
-        assertThat(lines.get(0)).isEqualTo("Id,Name,Title,def");
+        assertThat(lines.get(0)).isEqualTo("Id,Name,Title,Block1->def");
         assertThat(lines.get(1)).isEqualTo("id1,name1,title1,one");
     }
 
@@ -141,8 +159,11 @@ public class SearchIncludeFragmentTest {
 
         when(this.searchService.search(any(), any(), anyString(), any(), any(), any(),
                 any(), anyInt(), anyInt(), anyBoolean())).thenReturn(responseOf());
+        when(this.searchService.search(any(SolrQuery.class))).thenReturn(asList());
         when(this.datasetFieldTypeRepo.findAll())
                 .thenReturn(asList(this.type1, this.type2));
+        when(this.metadataBlockRepo.findSystemMetadataBlocks())
+                .thenReturn(asList(this.metaBlock));
 
         this.fragment.search();
         StreamedContent file = this.fragment.getSearchResultsFile();
@@ -150,11 +171,11 @@ public class SearchIncludeFragmentTest {
         assertThat(file.getContentEncoding()).isEqualTo("utf-8");
         assertThat(file.getContentType()).isEqualTo("text/csv");
         assertThat(file.getName()).isEqualTo("searchResults.csv");
-        assertThat(file.getContentLength()).isEqualTo(19);
+        assertThat(file.getContentLength()).isEqualTo(27);
 
         List<String> lines = readLines(file.getStream(), "utf-8");
         assertThat(lines.size()).isEqualTo(1);
-        assertThat(lines.get(0)).isEqualTo("Id,Name,Title,def");
+        assertThat(lines.get(0)).isEqualTo("Id,Name,Title,Block1->def");
     }
 
     @Test
