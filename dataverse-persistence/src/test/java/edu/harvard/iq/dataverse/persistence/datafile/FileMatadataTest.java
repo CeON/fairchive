47aad05ed156c88fd1333a75d39c84490fa3bf77
lbownik <lbownik@icm.edu.pl>
2025-03-13 09:16:15 +0100

    added tests for  addCategoryByName

--- dataverse-persistence/src/test/java/edu/harvard/iq/dataverse/persistence/datafile/FileMatadataTest.java
+++ dataverse-persistence/src/test/java/edu/harvard/iq/dataverse/persistence/datafile/FileMatadataTest.java
@@ -1,11 +1,15 @@
 package edu.harvard.iq.dataverse.persistence.datafile;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.Timestamp;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.harvard.iq.dataverse.persistence.dataset.Dataset;
+import edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion;
 
 public class FileMatadataTest {
 
@@ -74,5 +78,64 @@ public class FileMatadataTest {
 
         assertThat(fileMeta.getFileDateToDisplay())
                 .isEqualTo(fileMeta.getDataFile().getPublicationDate());
+    }
+    
+    @Test
+    public void addingCategoryByName_withNullArgument_doesNothing() {
+        FileMetadata meta = new FileMetadata();
+        
+        meta.addCategoryByName(null);
+        
+        assertThat(meta.getCategories()).isEmpty();
+    }
+    
+    @Test
+    public void addingCategoryByName_withEmptyArgument_doesNothing() {
+        FileMetadata meta = new FileMetadata();
+        
+        meta.addCategoryByName("");
+        
+        assertThat(meta.getCategories()).isEmpty();
+    }
+    
+    @Test
+    public void addingCategoryByName_whenItIsAlreadyPresent_doesNothing() {
+        FileMetadata meta = new FileMetadata();
+        meta.addCategory(this.category1);
+        
+        meta.addCategoryByName(this.category1.getName());
+        
+        assertThat(meta.getCategories().size()).isEqualTo(1);
+        assertThat(meta.getCategories().get(0)).isSameAs(this.category1);
+    }
+    
+    @Test
+    public void addingCategoryByName_whenItAlreadyExistsinDataset_addsExistingCategory() {
+        FileMetadata meta = new FileMetadata();
+        meta.setDatasetVersion(new DatasetVersion());
+        meta.getDatasetVersion().setDataset(new Dataset());
+        meta.getDatasetVersion().getDataset().setFileCategories(asList(this.category2));
+        meta.addCategory(this.category1);
+        
+        meta.addCategoryByName(this.category2.getName());
+        
+        assertThat(meta.getCategories().size()).isEqualTo(2);
+        assertThat(meta.getCategories().get(0)).isSameAs(this.category1);
+        assertThat(meta.getCategories().get(1)).isSameAs(this.category2);
+    }
+    
+    @Test
+    public void addingCategoryByName_withNeName_addsNewCategory() {
+        FileMetadata meta = new FileMetadata();
+        meta.setDatasetVersion(new DatasetVersion());
+        meta.getDatasetVersion().setDataset(new Dataset());
+        meta.getDatasetVersion().getDataset().setFileCategories(asList(this.category2));
+        meta.addCategory(this.category1);
+        
+        meta.addCategoryByName("NewCategory");
+        
+        assertThat(meta.getCategories().size()).isEqualTo(2);
+        assertThat(meta.getCategories().get(0)).isSameAs(this.category1);
+        assertThat(meta.getCategories().get(1).getName()).isSameAs("NewCategory");
     }
 }
