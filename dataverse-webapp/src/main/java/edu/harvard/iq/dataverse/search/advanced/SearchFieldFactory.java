e09eb4bdc7c4a5f035c2b9eceb0b90f19785e4cf
rscipien <rscipien@gmail.com>
2025-04-15 09:19:13 +0200

    Closes #2735: Advanced search add textbox to type coordinates (#2737)
    * Closes #2735: Advanced search add textbox to type coordinates
    
    * Closes #2735: Advanced search add textbox to type coordinates
    
    * Closes #2718: Move initial validation to parseCoordinates
    
    ---------
    
    Co-authored-by: Rafał Ścipień <rscipien@icm.edu.pl>

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/search/advanced/SearchFieldFactory.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/search/advanced/SearchFieldFactory.java
@@ -28,7 +28,7 @@ public class SearchFieldFactory {
             return new DateSearchField(fieldType);
         } else if (fieldType.isNumberic()) {
             return new NumberSearchField(fieldType);
-        } else if (fieldType.hasGeospatialAsParent()) {
+        } else if (fieldType.isGeoboxField()) {
             return new GeoboxCoordSearchField(fieldType);
         } else if(fieldType.isPeriodo()) {
             return new PeriodoSearchField(fieldType);
