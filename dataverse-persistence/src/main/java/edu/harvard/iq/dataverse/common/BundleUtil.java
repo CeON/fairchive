250d477edec01c78cb5b5756f66f8b856b5a42e5
Rafał Ścipień <rscipien@icm.edu.pl>
2025-05-14 12:19:07 +0200

    Closes #2759: Refactor getting cached bundle

--- dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/common/BundleUtil.java
+++ dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/common/BundleUtil.java
@@ -25,7 +25,7 @@ public class BundleUtil {
 
     private static final String DEFAULT_BUNDLE_FILE = "Bundle";
 
-    private static final String EXTENSION_PREFIX = "extension";
+    private static final String EXTENSION_SUFFIX = "extension";
 
     private static final Set<String> INTERNAL_BUNDLE_NAMES = Sets.newHashSet(
             DEFAULT_BUNDLE_FILE, "BuiltInRoles", "MimeTypeDisplay", "MimeTypeFacets", "ValidationMessages");
@@ -107,7 +107,7 @@ public class BundleUtil {
     }
 
     private static String getStringFromInternalBundle(String bundleKey, String bundleName, Locale locale) {
-        String displayNameFromExtensionBundle = getStringFromInternalBundle(bundleKey, bundleName, EXTENSION_PREFIX, locale);
+        String displayNameFromExtensionBundle = getStringFromInternalBundle(bundleKey, bundleName, EXTENSION_SUFFIX, locale);
         if (StringUtils.isNotBlank(displayNameFromExtensionBundle)) {
             return  displayNameFromExtensionBundle;
         }
@@ -116,21 +116,13 @@ public class BundleUtil {
     }
 
     private static String getStringFromInternalBundle(String bundleKey, String bundleName, String extension, Locale locale) {
-        String key = bundleName + "_" + locale.getLanguage();
-        if (StringUtils.isNotBlank(extension)) {
-            key = bundleName + "_" + extension + "_" + locale.getLanguage();
-            bundleName = bundleName + "_" + extension;
-        }
+        String bundleNameToUse = StringUtils.isNotBlank(extension)
+                ? bundleName + "_" + extension
+                : bundleName;
 
-        ResourceBundle resourceBundle = bundleCache.get(key);
-        if (resourceBundle == null) {
-            try {
-                resourceBundle = ResourceBundle.getBundle(bundleName, locale);
-            } catch (MissingResourceException mre) {
-                resourceBundle = EMPTY_BUNDLE;
-            }
-            bundleCache.putIfAbsent(key, resourceBundle);
-        }
+        String cacheKey = bundleNameToUse + "_" + locale.getLanguage();
+        ResourceBundle resourceBundle = getCachedBundle(cacheKey, bundleNameToUse, locale);
+
         try {
             return !EMPTY_BUNDLE.equals(resourceBundle)
                     ? resourceBundle.getString(bundleKey)
@@ -139,6 +131,19 @@ public class BundleUtil {
             logger.finest("Could not find key \"" + bundleKey + "\" in bundle file: " + bundleName);
             return StringUtils.EMPTY;
         }
+    }
+
+    private static ResourceBundle getCachedBundle(String cacheKey, String bundleName, Locale locale) {
+        ResourceBundle resourceBundle = bundleCache.get(cacheKey);
+        if (resourceBundle == null) {
+            try {
+                resourceBundle = ResourceBundle.getBundle(bundleName, locale);
+            } catch (MissingResourceException mre) {
+                resourceBundle = EMPTY_BUNDLE;
+            }
+            bundleCache.putIfAbsent(cacheKey, resourceBundle);
+        }
+        return resourceBundle;
     }
 
     // IMPORTANT: this method is nearly exact copy of getStringFromInternalBundle(…), however
