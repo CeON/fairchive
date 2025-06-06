e1c2bac1d0b14662cfd436e2ef978195fe6aa6a3
Rafał Ścipień <rscipien@icm.edu.pl>
2025-06-06 08:14:01 +0200

    Closes #2813: Clear validation message for subfields with conditaional rendering enabled

--- fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/dataset/metadata/inputRenderer/VocabSelectInputFieldRendererFactoryTest.java
+++ fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/dataset/metadata/inputRenderer/VocabSelectInputFieldRendererFactoryTest.java
@@ -7,10 +7,13 @@ import edu.harvard.iq.dataverse.persistence.dataset.DatasetField;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldType;
 import edu.harvard.iq.dataverse.persistence.dataset.MetadataBlock;
 import edu.harvard.iq.dataverse.util.json.TestJsonCreator;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -74,5 +77,41 @@ public class VocabSelectInputFieldRendererFactoryTest {
         VocabSelectInputFieldRenderer renderer = inputFieldRendererFactory.createRenderer(fieldType, rendererOptions);
         // then
         assertTrue(renderer.showOnCondition(subfields));
+    }
+
+    @Test
+    public void createRenderer__clearValidationMessage() {
+        // given
+        JsonObject rendererOptionsConditionalRendering = TestJsonCreator
+                .stringAsJsonElement("{'conditionalRendering': {'datasetFieldName':'country', 'renderOnValue':'Poland'}}")
+                .getAsJsonObject();
+        JsonObject rendererOptionsEmpty = TestJsonCreator
+                .stringAsJsonElement("{}")
+                .getAsJsonObject();
+        DatasetField mainField = new DatasetField();
+        DatasetField parent = new DatasetField();
+        List<DatasetField> subfields = new ArrayList<>();
+        DatasetField field = new DatasetField();
+        DatasetFieldType typeMain = MocksFactory.makeControlledVocabDatasetFieldType("country",
+                false,
+                new MetadataBlock(),
+                "Poland");
+        DatasetFieldType typeSubField = MocksFactory.makeControlledVocabDatasetFieldType("test", false, new MetadataBlock(), "test");
+        field.setDatasetFieldType(typeSubField);
+        field.setSingleControlledVocabularyValue(new ControlledVocabularyValue(0L, "Poland", null));
+        field.setValidationMessage("Error");
+        subfields.add(field);
+        parent.setDatasetFieldsChildren(subfields);
+        mainField.setDatasetFieldParent(parent);
+        mainField.setDatasetFieldType(typeMain);
+        Map<DatasetFieldType, InputFieldRenderer> map = new HashMap<>();
+        map.put(typeMain, inputFieldRendererFactory.createRenderer(typeMain, rendererOptionsEmpty));
+        map.put(typeSubField, inputFieldRendererFactory.createRenderer(typeMain, rendererOptionsConditionalRendering));
+
+        // when
+        VocabSelectInputFieldRenderer renderer = inputFieldRendererFactory.createRenderer(fieldType, rendererOptionsEmpty);
+        renderer.processValueChange(mainField, map);
+        // then
+        assertTrue(subfields.stream().allMatch(df -> StringUtils.isBlank(df.getValidationMessage())));
     }
 }
