3421753b9cbda4661833043224a307ea5fed1e2b
lbownik <lbownik@icm.edu.pl>
2025-06-10 14:18:21 +0200

    Closes #2787 Advanced search for geonames

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/search/SearchFields.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/search/SearchFields.java
@@ -272,5 +272,8 @@ public class SearchFields {
     public static final String PERIODO_LOCATIONS = "periodo_locations_ss";
 
     public static final String GEOGRAPHIC_COORDINATES = SearchDynamicFieldPrefix.TEXT.getPrefix() + "geographicCoordinates";
+    
+    public static final String GEONAME_ID = "geoname_id_s";
+    public static final String GEONAME_NAME = "geoname_name_txt";
 
 }
