fadc8db8ea886ac018714fbbba75746a9b2d727e
Rafał Ścipień <rscipien@icm.edu.pl>
2025-07-09 11:54:45 +0200

    Closes #2837: Detail suggestion handler from getDetails remove parsing json, no need in base class

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/metadata/inputRenderer/suggestion/ControlledVocabularyDetailsSuggestionHandler.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/metadata/inputRenderer/suggestion/ControlledVocabularyDetailsSuggestionHandler.java
@@ -1,11 +1,9 @@
 package edu.harvard.iq.dataverse.dataset.metadata.inputRenderer.suggestion;
 
 import com.google.common.collect.Lists;
-import com.google.gson.Gson;
 import edu.harvard.iq.dataverse.ControlledVocabularyValueServiceBean;
 import edu.harvard.iq.dataverse.dataset.metadata.inputRenderer.Suggestion;
 import edu.harvard.iq.dataverse.persistence.dataset.ControlledVocabularyValue;
-import io.vavr.control.Try;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
@@ -62,13 +60,8 @@ public class ControlledVocabularyDetailsSuggestionHandler implements SuggestionH
      * @return can return html
      */
     public String getDetails(ControlledVocabularyValue controlledVocabularyValue) {
-        if (StringUtils.isNotBlank(controlledVocabularyValue.getSuggestionDetails())) {
-            List details = Try.of(() -> new Gson().fromJson(controlledVocabularyValue.getSuggestionDetails(), List.class))
-                .getOrElseThrow((e) -> new IllegalArgumentException("Invalid json of suggestion details " + controlledVocabularyValue.getSuggestionDetails() + ")", e));
-
-            return String.join(",", details);
-        }
-
-        return controlledVocabularyValue.getStrValue();
+        return StringUtils.isNotBlank(controlledVocabularyValue.getSuggestionDetails()) ?
+                controlledVocabularyValue.getSuggestionDetails() :
+                controlledVocabularyValue.getStrValue();
     }
 }
