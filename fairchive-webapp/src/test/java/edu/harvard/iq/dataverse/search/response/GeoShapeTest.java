c61c349645f78e5e6d924e4dba4ff50df7bbb4fe
Rafał Ścipień <rscipien@icm.edu.pl>
2025-07-10 12:47:13 +0200

    Closes #2840: Create GeoShape class to represent any shape that can be created on map and move calculation center of polygon

---
+++ fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/search/response/GeoShapeTest.java
@@ -0,0 +1,62 @@
+package edu.harvard.iq.dataverse.search.response;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoShapeTest {
+
+
+
+    @Test
+    public void calculateCenter__no_points() {
+        // given
+        List<GeoPoint> coordinates = new ArrayList<>();
+
+        //when & then
+        assertThrows(IllegalArgumentException.class, () -> GeoShape.of(coordinates));
+    }
+
+    @Test
+    public void calculateCenter_one_point() {
+        // given & when
+        List<GeoPoint> coordinates = new ArrayList<>();
+        coordinates.add(new GeoPoint(1,2));
+        GeoShape result =  GeoShape.of(coordinates);
+
+        // then
+        assertThat(result.getCenter()).isEqualTo(new GeoPoint(1, 2));
+    }
+
+    @Test
+    public void calculateCenter_two_points() {
+        // given & when
+        List<GeoPoint> coordinates = new ArrayList<>();
+        coordinates.add(new GeoPoint(1,2));
+        coordinates.add(new GeoPoint(5,8));
+        GeoShape result =  GeoShape.of(coordinates);
+
+        // then
+        assertThat(result.getCenter()).isEqualTo(new GeoPoint(3, 5));
+    }
+
+    @Test
+    public void calculateCenter_multiple_points() {
+        // given & when
+        List<GeoPoint> coordinates = new ArrayList<>();
+        coordinates.add(new GeoPoint(21.82144,50.17739));
+        coordinates.add(new GeoPoint(21.82144,22.21139));
+        coordinates.add(new GeoPoint(49.85973,22.21139));
+        coordinates.add(new GeoPoint(49.85973,50.17739));
+        coordinates.add(new GeoPoint(21.82144,50.17739));
+        GeoShape result =  GeoShape.of(coordinates);
+
+        // then
+        assertThat(result.getCenter()).isEqualTo(new GeoPoint(35.840585,36.19439));
+    }
+
+}
