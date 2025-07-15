fc83ee81db034a51e5c47331242e1bb33fb0954a
lbownik <lbownik@icm.edu.pl>
2025-07-15 10:45:29 +0200

    various refactorings

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dashboard/DashboardMaximumEmbargoPage.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dashboard/DashboardMaximumEmbargoPage.java
@@ -5,20 +5,18 @@ import static org.apache.commons.lang.StringUtils.EMPTY;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.apache.commons.lang.StringUtils;
 import org.omnifaces.cdi.ViewScoped;
 
 import edu.harvard.iq.dataverse.DataverseDao;
 import edu.harvard.iq.dataverse.DataverseSession;
-import edu.harvard.iq.dataverse.PermissionsWrapper;
+import edu.harvard.iq.dataverse.NavigationWrapper;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 import edu.harvard.iq.dataverse.settings.SettingsServiceBean.Key;
-import edu.harvard.iq.dataverse.util.JsfHelper;
-import edu.harvard.iq.dataverse.util.SystemConfig;
-import io.vavr.control.Try;
+import edu.harvard.iq.dataverse.util.UIMessages;
 
 @SuppressWarnings("serial")
 @ViewScoped
@@ -27,9 +25,9 @@ public class DashboardMaximumEmbargoPage implements Serializable {
 
     private SettingsServiceBean settings;
     private DataverseSession session;
-    private PermissionsWrapper permissionsWrapper;
+    private NavigationWrapper navigation;
     private DataverseDao dataverseDao;
-    private SystemConfig config;
+    private UIMessages ui;
 
     private boolean isMaximumEmbargoSet;
     private int maximumEmbargoLength;
@@ -43,14 +41,14 @@ public class DashboardMaximumEmbargoPage implements Serializable {
     @Inject
     public DashboardMaximumEmbargoPage(final SettingsServiceBean settings, 
                                     final DataverseSession session,
-                                    final PermissionsWrapper permissionsWrapper,
+                                    final NavigationWrapper navigation,
                                     final DataverseDao dataverseDao,
-                                    final SystemConfig config) {
+                                    final UIMessages ui) {
         this.settings = settings;
         this.session = session;
-        this.permissionsWrapper = permissionsWrapper;
+        this.navigation = navigation;
         this.dataverseDao = dataverseDao;
-        this.config = config;
+        this.ui = ui;
     }
 
     // -------------------- GETTERS --------------------
@@ -64,38 +62,35 @@ public class DashboardMaximumEmbargoPage implements Serializable {
     }
 
     // -------------------- LOGIC --------------------
-    public String init() {
-        if (!this.session.getUser().isSuperuser() || this.config.isReadonlyMode()) {
-            return this.permissionsWrapper.notAuthorized();
-        }
-
-        this.isMaximumEmbargoSet = this.settings.getValueForKeyAsInt(Key.MaximumEmbargoLength) > 0;
-        this.maximumEmbargoLength = this.settings.getValueForKeyAsInt(Key.MaximumEmbargoLength);
-
-        return EMPTY;
+    @PostConstruct
+    public void init() {
+        this.maximumEmbargoLength = this.settings
+                .getValueForKeyAsInt(Key.MaximumEmbargoLength);
+        this.isMaximumEmbargoSet = this.maximumEmbargoLength > 0;
+    }
+    
+    public String verifyAccess() {
+        return this.session.canEditDashboard() ? EMPTY : this.navigation.notAuthorized();
     }
 
     public String save() {
-        if(this.isMaximumEmbargoSet) {
-            setMaxEmbargoSetting(this.maximumEmbargoLength);
-        } else {
-            setMaxEmbargoSetting(0);
+        try {
+            final String maxEngth = this.isMaximumEmbargoSet
+                    ? Integer.toString(this.maximumEmbargoLength)
+                    : "0";
+            this.settings.setValueForKey(Key.MaximumEmbargoLength, maxEngth);
+            this.ui.addSuccessMessage(
+                    getStringFromBundle("dashboard.card.maximumembargo.save.success"));
+        } catch (final Exception e) {
+            this.ui.addErrorMessage(
+                    getStringFromBundle("dashboard.card.maximumembargo.save.failure"));
         }
-
         return EMPTY;
     }
 
-
     public String cancel() {
-        return "/dashboard.xhtml?dataverseId=" 
-                + this.dataverseDao.findRootDataverse().getId() + "&faces-redirect=true";
-    }
-
-    // -------------------- PRIVATE ---------------------
-    private void setMaxEmbargoSetting(final int maxLength) {
-        Try.of(() -> this.settings.setValueForKey(Key.MaximumEmbargoLength, Integer.toString(maxLength)))
-                .onSuccess(setting -> JsfHelper.addSuccessMessage(getStringFromBundle("dashboard.card.maximumembargo.save.success")))
-                .onFailure(setting -> JsfHelper.addErrorMessage(getStringFromBundle("dashboard.card.maximumembargo.save.failure")));
+        return "/dashboard.xhtml?faces-redirect=true&dataverseId=" 
+                + this.dataverseDao.findRootDataverse().getId();
     }
 
     // -------------------- SETTERS --------------------
