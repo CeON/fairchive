f2f80d75ff44685573a8aace19c65e3b1c5d9dbc
lbownik <lbownik@icm.edu.pl>
2025-08-22 14:51:18 +0200

    refactored test

--- fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/ingest/UningestInfoServiceTest.java
+++ fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/ingest/UningestInfoServiceTest.java
@@ -1,94 +1,126 @@
 package edu.harvard.iq.dataverse.ingest;
 
-import edu.harvard.iq.dataverse.persistence.MocksFactory;
+import static edu.harvard.iq.dataverse.persistence.datafile.DataFile.INGEST_STATUS_ERROR;
+import static edu.harvard.iq.dataverse.persistence.datafile.DataFile.INGEST_STATUS_NONE;
+import static edu.harvard.iq.dataverse.persistence.datafile.DataFile.INGEST_STATUS_SCHEDULED;
+import static edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion.VersionState.DRAFT;
+import static edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion.VersionState.RELEASED;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 import edu.harvard.iq.dataverse.persistence.datafile.DataFile;
 import edu.harvard.iq.dataverse.persistence.datafile.DataTable;
 import edu.harvard.iq.dataverse.persistence.datafile.FileMetadata;
 import edu.harvard.iq.dataverse.persistence.dataset.Dataset;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class UningestInfoServiceTest {
 
-    private IngestServiceBean ingestService = new IngestServiceBean();
-    private UningestInfoService service = new UningestInfoService(ingestService);
-
-
-    @ParameterizedTest
-    @CsvSource({
-            "true, text/xml-graphml, true, B, false",
-            "true, text/csv, false, A, true",
-            "true, text/csv, true, A, false",
-            "false, text/csv, true, A, true",
-            "false, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, true, D, false",
-            "false, text/tsv, true, A, true"
-    })
-    void listUningestableFiles_AND_hasUningestableFiles__variousFiles(boolean expectedEmpty, String mime, boolean singleVersion,
-                                             char ingestStatus, boolean hasDataTable) {
-        // given
-        Dataset dataset = prepareDatasetWithDataFile(mime, singleVersion, ingestStatus, hasDataTable ? new DataTable() : null);
-
-        // when
-        List<DataFile> files = service.listUningestableFiles(dataset);
-        boolean hasUningestableFiles = service.hasUningestableFiles(dataset);
-
-
-        // then
-        assertThat(files).hasSize(expectedEmpty ? 0 : 1);
-        assertThat(hasUningestableFiles).isEqualTo(!expectedEmpty);
-    }
+    private final IngestServiceBean ingestService = new IngestServiceBean();
+    private final UningestInfoService service = new UningestInfoService(ingestService);
 
     @Test
-    void listUningestableFiles_AND_hasUningestableFiles__uinngestableDatasets_releasedDataset() {
-        // given
-        Dataset releasedDataset = prepareDatasetWithDataFile("text/csv", true, 'D', null);
-        releasedDataset.getLatestVersion().setVersionState(DatasetVersion.VersionState.RELEASED);
-
-        // when
-        List<DataFile> files = service.listUningestableFiles(releasedDataset);
-        boolean hasUningestableFiles = service.hasUningestableFiles(releasedDataset);
-
-        // then
-        assertThat(files).isEmpty();
-        assertThat(hasUningestableFiles).isFalse();
+    void emptyDataset_hasNoUningestableFiles() {
+        Dataset set = createDataset();
+        
+        assertThat(this.service.hasUningestableFiles(set)).isFalse();
+        assertThat(this.service.listUningestableFiles(set)).isEmpty();
     }
-
+    
     @Test
-    void listUningestableFiles_AND_hasUningestableFiles__uinngestableDatasets_emptyDataset() {
-        // given
-        Dataset noFilesDataset = new Dataset();
-
-        // when
-        List<DataFile> files = service.listUningestableFiles(noFilesDataset);
-        boolean hasUningestableFiles = service.hasUningestableFiles(noFilesDataset);
-
-        // then
-        assertThat(files).isEmpty();
-        assertThat(hasUningestableFiles).isFalse();
+    void releasedDataset_hasNoUningestableFiles() {
+        Dataset set = createDataset();
+        addFile(set, "text/csv", INGEST_STATUS_SCHEDULED, null);
+        set.getLatestVersion().setVersionState(RELEASED);
+        
+        assertThat(this.service.hasUningestableFiles(set)).isFalse();
+        assertThat(this.service.listUningestableFiles(set)).isEmpty();
     }
-
-    // -------------------- PRIVATE --------------------
-
-    private Dataset prepareDatasetWithDataFile(String mime, boolean singleVersion, char ingestStatus, DataTable dataTable) {
-        Dataset dataset = MocksFactory.makeDataset();
-        DatasetVersion version = dataset.getLatestVersion();
-        DataFile file = dataset.getFiles().get(0);
-        FileMetadata fileMetadata = file.getFileMetadata();
-        fileMetadata.setDatasetVersion(version);
-        file.setContentType(mime);
-        file.setIngestStatus(ingestStatus);
-        file.setDataTable(dataTable);
-        if (!singleVersion) {
-            List<FileMetadata> metadatas = file.getFileMetadatas();
-            metadatas.add(metadatas.get(0));
-        }
-        dataset.getFiles().removeIf(f -> !file.equals(f));
+    
+    @Test
+    void draftDataset_withoutIngestableFiles_hasNoUningestableFiles() {
+        Dataset set = createDataset();
+        addFile(set, "text/xml-graphml", INGEST_STATUS_SCHEDULED, null);
+        
+        assertThat(this.service.hasUningestableFiles(set)).isFalse();
+        assertThat(this.service.listUningestableFiles(set)).isEmpty();
+    }
+    
+    @Test
+    void draftDataset_withoutIngestedFiles_hasNoUningestableFiles() {
+        Dataset set = createDataset();
+        addFile(set, "text/csv", INGEST_STATUS_NONE, null);
+        
+        assertThat(this.service.hasUningestableFiles(set)).isFalse();
+        assertThat(this.service.listUningestableFiles(set)).isEmpty();
+    }
+    
+    @Test
+    void draftDataset_withUningestedFiles_hasNoUningestableFiles() {
+        Dataset set = createDataset();
+        addFile(set, "text/csv", INGEST_STATUS_NONE, null);
+        
+        assertThat(this.service.hasUningestableFiles(set)).isFalse();
+        assertThat(this.service.listUningestableFiles(set)).isEmpty();
+    }
+    
+    @Test
+    void draftDataset_withMultipleVersionsIngestedFiles_hasNoUningestableFiles() {
+        Dataset set = createDataset();
+        DataFile file = addFile(set, "text/csv", INGEST_STATUS_NONE, new DataTable());
+        final FileMetadata meta = new FileMetadata();
+        meta.setDataFile(file);
+        file.getFileMetadatas().add(meta);
+        meta.setDatasetVersion(set.getLatestVersion());
+        set.getLatestVersion().addFileMetadata(meta);
+        
+        assertThat(this.service.hasUningestableFiles(set)).isFalse();
+        assertThat(this.service.listUningestableFiles(set)).isEmpty();
+    }
+    
+    @Test
+    void draftDataset_withIngestedFiles_hasUningestableFiles() {
+        Dataset set = createDataset();
+        addFile(set, "text/csv", INGEST_STATUS_NONE, new DataTable());
+        addFile(set, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", INGEST_STATUS_ERROR, null);
+        addFile(set, "text/tsv", INGEST_STATUS_NONE, new DataTable());
+        
+        assertThat(this.service.hasUningestableFiles(set)).isTrue();
+        List<DataFile> files = this.service.listUningestableFiles(set);
+        assertThat(files).hasSize(3);
+        assertThat(files.stream().map(DataFile::getContentType))
+            .containsExactly("text/csv", 
+                             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+                             "text/tsv");
+    }
+    
+    private Dataset createDataset() {
+        final Dataset dataset = new Dataset();
+        
+        final DatasetVersion version = new DatasetVersion(); 
+        version.setDataset(dataset);
+        version.setVersionState(DRAFT);
+        dataset.getVersions().add(version);
+        
         return dataset;
+    }
+    
+    private DataFile addFile(final Dataset set, final String mimeType, final char ingestStatus, final DataTable table) {
+        final DataFile file = new DataFile();
+        
+        set.getFiles().add(file);
+        final FileMetadata meta = new FileMetadata();
+        file.getFileMetadatas().add(meta);
+        set.getLatestVersion().addFileMetadata(meta);
+        meta.setDatasetVersion(set.getLatestVersion());
+        meta.setDataFile(file);
+        file.setContentType(mimeType);
+        file.setIngestStatus(ingestStatus);
+        file.setDataTable(table);
+        
+        return file;
     }
 }
