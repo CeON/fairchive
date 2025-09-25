4c293723164b6b421894530ba88ceaac019ab08d
lbownik <lbownik@icm.edu.pl>
2025-09-25 12:48:59 +0200

    removed unused method

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/DatasetDao.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/DatasetDao.java
@@ -342,15 +342,6 @@ public class DatasetDao implements java.io.Serializable {
         return list.size() == 1 ? list.get(0) : null;
     }
 
-    public Long getDatasetVersionCardImage(Long versionId, User user) {
-        if (versionId == null) {
-            return null;
-        }
-
-
-        return null;
-    }
-
     public void updateLastExportTimeStamp(Long datasetId) {
         Date now = new Date();
         em.createNativeQuery("UPDATE Dataset SET lastExportTime='" + now.toString() + "' WHERE id=" + datasetId).executeUpdate();
