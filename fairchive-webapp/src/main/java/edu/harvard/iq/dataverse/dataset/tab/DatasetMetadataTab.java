d46df30b1720f1dbd166db1c39255f91a2edb95f
jnejman <jnejman@icm.edu.pl>
2025-10-01 11:39:50 +0200

    Closes #128: Machine translation for metadata

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/tab/DatasetMetadataTab.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/tab/DatasetMetadataTab.java
@@ -126,6 +126,62 @@ public class DatasetMetadataTab implements Serializable {
         return datasetDao.find(dataset.getId()).getAlternativePersistentIdentifier();
     }
 
+    /**
+     * Extracts exporters display name and redirect url.
+     */
+    public List<Tuple2<String, String>> getTranslationLanguages() {
+        List<Tuple2<String, String>> result = new ArrayList<>();
+        result.add(Tuple.of("sq","Albanian"));
+        result.add(Tuple.of("ar","Arabic"));
+        result.add(Tuple.of("az","Azerbaijani"));
+        result.add(Tuple.of("eu","Basque"));
+        result.add(Tuple.of("bn","Bengali"));
+        result.add(Tuple.of("bg","Bulgarian"));
+        result.add(Tuple.of("ca","Catalan"));
+        result.add(Tuple.of("zh-Hans","Chinese"));
+        result.add(Tuple.of("zh-Hant","Chinese (traditional)"));
+        result.add(Tuple.of("cs","Czech"));
+        result.add(Tuple.of("da","Danish"));
+        result.add(Tuple.of("nl","Dutch"));
+        result.add(Tuple.of("en","English"));
+        result.add(Tuple.of("eo","Esperanto"));
+        result.add(Tuple.of("et","Estonian"));
+        result.add(Tuple.of("fi","Finnish"));
+        result.add(Tuple.of("fr","French"));
+        result.add(Tuple.of("gl","Galician"));
+        result.add(Tuple.of("de","German"));
+        result.add(Tuple.of("el","Greek"));
+        result.add(Tuple.of("he","Hebrew"));
+        result.add(Tuple.of("hi","Hindi"));
+        result.add(Tuple.of("hu","Hungarian"));
+        result.add(Tuple.of("id","Indonesian"));
+        result.add(Tuple.of("ga","Irish"));
+        result.add(Tuple.of("it","Italian"));
+        result.add(Tuple.of("ja","Japanese"));
+        result.add(Tuple.of("ko","Korean"));
+        result.add(Tuple.of("ky","Kyrgyz"));
+        result.add(Tuple.of("lv","Latvian"));
+        result.add(Tuple.of("lt","Lithuanian"));
+        result.add(Tuple.of("ms","Malay"));
+        result.add(Tuple.of("nb","Norwegian"));
+        result.add(Tuple.of("fa","Persian"));
+        result.add(Tuple.of("pl","Polish"));
+        result.add(Tuple.of("pt","Portuguese"));
+        result.add(Tuple.of("pt-BR","Portuguese (Brazil)"));
+        result.add(Tuple.of("ro","Romanian"));
+        result.add(Tuple.of("ru","Russian"));
+        result.add(Tuple.of("sk","Slovak"));
+        result.add(Tuple.of("sl","Slovenian"));
+        result.add(Tuple.of("es","Spanish"));
+        result.add(Tuple.of("sv","Swedish"));
+        result.add(Tuple.of("tl","Tagalog"));
+        result.add(Tuple.of("th","Thai"));
+        result.add(Tuple.of("tr","Turkish"));
+        result.add(Tuple.of("uk","Ukrainian"));
+        result.add(Tuple.of("ur","Urdu"));
+        return result;
+    }
+
     // -------------------- PRIVATE --------------------
 
     private String createExporterURL(Exporter exporter, String myHostURL) {
