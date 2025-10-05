e12e6c01320e50d2e0a6bb247a55fe5f2c0216ca
lbownik <lbownik@icm.edu.pl>
2025-10-05 19:37:23 +0200

    optimized performance of ror validator (reduced execution time 8x and reduced allocations about 10x - because it's fun: ) )

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/validation/RorValidator.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/validation/RorValidator.java
@@ -4,58 +4,42 @@ import javax.ejb.Stateless;
 
 import static edu.harvard.iq.dataverse.validation.ValidationResult.invalid;
 import static edu.harvard.iq.dataverse.validation.ValidationResult.ok;
-import static java.lang.Long.parseLong;
-import static java.util.Arrays.stream;
-import static java.util.Collections.unmodifiableMap;
+import static java.util.Arrays.binarySearch;
 import static java.util.regex.Pattern.compile;
-import static java.util.stream.Collectors.toMap;
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
-import java.util.Map;
-import java.util.function.Function;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Stateless
 public class RorValidator {
     
-    public static final String INVALID_FORMAT = "ror.invalid.format";
-    public static final String INVALID_CHECKSUM = "ror.invalid.checksum";
-    private static final Map<String, Integer> SYMBOL_VALUES = symbolValues();
+    private static final char[] SYMBOL_VALUES = 
+        {'0','1','2','3','4','5','6','7','8','9',
+        'a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','v','w','x','y','z'};
     private static final Pattern FORMAT_PATTERN = 
             compile("https://ror\\.org/0[a-hjkmnp-tv-z0-9]{6}[0-9]{2}");
 
-    // -------------------- LOGIC --------------------
-
     public ValidationResult validate(final String fullRor) {
-        if (isBlank(fullRor)) {
-            return invalid(INVALID_FORMAT);
+        if (fullRor != null && FORMAT_PATTERN.matcher(fullRor).matches()) {
+            return isChecksumValid(fullRor)
+                    ? ok()
+                    : invalid("ror.invalid.checksum");
+        } else {
+            return invalid("ror.invalid.format");
         }
-        final Matcher matcher = FORMAT_PATTERN.matcher(fullRor);
-        if (!matcher.matches()) {
-            return invalid(INVALID_FORMAT);
-        }
-        final String value = fullRor.substring(fullRor.lastIndexOf("/") + 1);
-        final String encoded = value.substring(0, 7);
-        final long checksum = parseLong(value.substring(7));
-        return checksum == computeChecksum(encoded)
-                ? ok()
-                : invalid(INVALID_CHECKSUM);
     }
 
-    // -------------------- PRIVATE --------------------
-
-    private static long computeChecksum(final String encoded) {
-        long decoded = stream(encoded.split(""))
-                .mapToLong(SYMBOL_VALUES::get)
-                .reduce(0L, (accumulated, element) -> 32L * accumulated + element);
-        return 98L - ((decoded * 100L) % 97);
-    }
-
-    private static Map<String, Integer> symbolValues() {
-        final String symbols = "0123456789abcdefghjkmnpqrstvwxyz";
-        return unmodifiableMap(
-                stream(symbols.split(""))
-                        .collect(toMap(Function.identity(), symbols::indexOf)));
+    private static boolean isChecksumValid(final String fullRor) {
+        int index = fullRor.lastIndexOf('/') + 1;
+        final int end = index + 7;
+        long decoded = 0;
+        while (index < end) {
+            decoded = 32L * decoded + binarySearch(SYMBOL_VALUES, fullRor.charAt(index));
+            ++index;
+        }
+        decoded = 98L - ((decoded * 100L) % 97);
+        
+        final long checksum = 10*(fullRor.charAt(index) - '0') +
+                (fullRor.charAt(++index) - '0');
+        return decoded == checksum;
     }
 }
