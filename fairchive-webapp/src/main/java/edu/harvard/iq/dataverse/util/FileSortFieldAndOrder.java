0210d005618911ab7fbd6ed7d8926ab4ef8b5d9f
lbownik <lbownik@icm.edu.pl>
2025-10-13 10:20:07 +0200

    comment addressed

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/util/FileSortFieldAndOrder.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/util/FileSortFieldAndOrder.java
@@ -9,11 +9,11 @@ public class FileSortFieldAndOrder {
     private final String sortField;
     private final SortOrder sortOrder;
 
-    private static String displayOrder = "displayOrder";
-    private static String label = "label";
-    private static String createDate = "dataFile.createDate";
-    private static String size = "dataFile.filesize";
-    private static String type = "dataFile.contentType";
+    private final static String displayOrder = "displayOrder";
+    private final static String label = "label";
+    private final static String createDate = "dataFile.createDate";
+    private final static String size = "dataFile.filesize";
+    private final static String type = "dataFile.contentType";
 
     public FileSortFieldAndOrder(final String userSuppliedSortField, 
             final SortOrder userSuppliedSortOrder) {
