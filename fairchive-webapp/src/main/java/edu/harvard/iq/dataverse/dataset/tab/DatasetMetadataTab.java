b72693171c0800d97c1e4bd9d7701fc04b2dbc21
JakubNejmanICM <168078820+JakubNejmanICM@users.noreply.github.com>
2025-10-29 12:38:11 +0100

    Closes #128: Machine translation for metadata (#2985)
    * Closes #128: Machine translation for metadata
    
    * Code review implementations
    
    * Code review implementations
    
    * Code review implementations
    
    * Merge

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/tab/DatasetMetadataTab.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/tab/DatasetMetadataTab.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 import org.omnifaces.cdi.ViewScoped;
 
 import edu.harvard.iq.dataverse.DataverseSession;
@@ -26,6 +27,9 @@ import edu.harvard.iq.dataverse.util.SystemConfig;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
 
+import static edu.harvard.iq.dataverse.settings.SettingsServiceBean.Key.ShowMachineTranslation;
+import static edu.harvard.iq.dataverse.settings.SettingsServiceBean.Key.MachineTranslationURL;
+
 @SuppressWarnings("serial")
 @ViewScoped
 @Named("DatasetMetadataTab")
@@ -41,6 +45,7 @@ public class DatasetMetadataTab implements Serializable {
     private Dataset dataset;
     private boolean isDatasetLocked;
     private Map<MetadataBlock, List<DatasetFieldsByType>> metadataBlocks;
+    private SettingsServiceBean settingService;
 
     // -------------------- CONSTRUCTORS --------------------
 
@@ -54,13 +59,15 @@ public class DatasetMetadataTab implements Serializable {
                               ExportService exportService,
                               SystemConfig systemConfig,
                               DatasetFieldsInitializer datasetVersionUI,
-                              DatasetService datasetService) {
+                              DatasetService datasetService,
+                              SettingsServiceBean settingService) {
         this.permissionsWrapper = permissionsWrapper;
         this.session = session;
         this.exportService = exportService;
         this.systemConfig = systemConfig;
         this.datasetFieldsInitializer = datasetVersionUI;
         this.datasetService = datasetService;
+        this.settingService = settingService;
     }
 
     // -------------------- GETTERS --------------------
@@ -124,6 +131,70 @@ public class DatasetMetadataTab implements Serializable {
 
     public String getAlternativePersistentIdentifier() {
         return datasetService.find(dataset.getId()).getAlternativePersistentIdentifier();
+    }
+
+    /**
+     * Extracts exporters display name and redirect url.
+     */
+    public List<Tuple2<String, String>> getTranslationLanguages() {
+        List<Tuple2<String, String>> result = new ArrayList<>();
+        result.add(Tuple.of("sq","Albanian"));
+        result.add(Tuple.of("ar","Arabic"));
+        result.add(Tuple.of("az","Azerbaijani"));
+        result.add(Tuple.of("eu","Basque"));
+        result.add(Tuple.of("bn","Bengali"));
+        result.add(Tuple.of("bg","Bulgarian"));
+        result.add(Tuple.of("ca","Catalan"));
+        result.add(Tuple.of("zh-Hans","Chinese"));
+        result.add(Tuple.of("zh-Hant","Chinese (traditional)"));
+        result.add(Tuple.of("cs","Czech"));
+        result.add(Tuple.of("da","Danish"));
+        result.add(Tuple.of("nl","Dutch"));
+        result.add(Tuple.of("en","English"));
+        result.add(Tuple.of("eo","Esperanto"));
+        result.add(Tuple.of("et","Estonian"));
+        result.add(Tuple.of("fi","Finnish"));
+        result.add(Tuple.of("fr","French"));
+        result.add(Tuple.of("gl","Galician"));
+        result.add(Tuple.of("de","German"));
+        result.add(Tuple.of("el","Greek"));
+        result.add(Tuple.of("he","Hebrew"));
+        result.add(Tuple.of("hi","Hindi"));
+        result.add(Tuple.of("hu","Hungarian"));
+        result.add(Tuple.of("id","Indonesian"));
+        result.add(Tuple.of("ga","Irish"));
+        result.add(Tuple.of("it","Italian"));
+        result.add(Tuple.of("ja","Japanese"));
+        result.add(Tuple.of("ko","Korean"));
+        result.add(Tuple.of("ky","Kyrgyz"));
+        result.add(Tuple.of("lv","Latvian"));
+        result.add(Tuple.of("lt","Lithuanian"));
+        result.add(Tuple.of("ms","Malay"));
+        result.add(Tuple.of("nb","Norwegian"));
+        result.add(Tuple.of("fa","Persian"));
+        result.add(Tuple.of("pl","Polish"));
+        result.add(Tuple.of("pt","Portuguese"));
+        result.add(Tuple.of("pt-BR","Portuguese (Brazil)"));
+        result.add(Tuple.of("ro","Romanian"));
+        result.add(Tuple.of("ru","Russian"));
+        result.add(Tuple.of("sk","Slovak"));
+        result.add(Tuple.of("sl","Slovenian"));
+        result.add(Tuple.of("es","Spanish"));
+        result.add(Tuple.of("sv","Swedish"));
+        result.add(Tuple.of("tl","Tagalog"));
+        result.add(Tuple.of("th","Thai"));
+        result.add(Tuple.of("tr","Turkish"));
+        result.add(Tuple.of("uk","Ukrainian"));
+        result.add(Tuple.of("ur","Urdu"));
+        return result;
+    }
+
+    public boolean showMachineTranslation() {
+        return settingService.isTrueForKey(ShowMachineTranslation);
+    }
+
+    public String getMachineTranslationURL() {
+        return settingService.getValueForKey(MachineTranslationURL);
     }
 
     // -------------------- PRIVATE --------------------
