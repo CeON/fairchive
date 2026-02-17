6261ff745427be1940c752654f39857d0fa33eba
lbownik <lbownik@icm.edu.pl>
2026-02-17 11:32:00 +0100

    style improvements of test class

--- fairchive-persistence/src/test/java/edu/harvard/iq/dataverse/persistence/user/AuthenticatedUserRepositoryIT.java
+++ fairchive-persistence/src/test/java/edu/harvard/iq/dataverse/persistence/user/AuthenticatedUserRepositoryIT.java
@@ -1,140 +1,103 @@
 package edu.harvard.iq.dataverse.persistence.user;
 
 import edu.harvard.iq.dataverse.persistence.PersistenceArquillianDeployment;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.*;
 
 import javax.inject.Inject;
 import java.util.List;
 
 import static edu.harvard.iq.dataverse.persistence.user.AuthenticatedUserRepository.SortKey;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthenticatedUserRepositoryIT extends PersistenceArquillianDeployment {
+	
+	private final static int resultLimit10 = 10;
+	private final static int offset0 = 0;
+	private final static boolean sortAscending = true;
+	private final static boolean sortDescenting = false;
 
     @Inject
-    private AuthenticatedUserRepository authenticatedUserRepository;
+    private AuthenticatedUserRepository repo;
 
     //-------------------- TESTS --------------------
 
     @Test
     public void findSearchedAuthenticatedUsers() {
-        // given
-        SortKey sortKey = SortKey.ID;
-        int resultLimit = 10;
-        int offset = 0;
-        String searchTerm = StringUtils.EMPTY;
-        boolean isSortAscending = true;
+    	
+        List<AuthenticatedUser> users = this.repo.findSearchedAuthenticatedUsers(
+        		SortKey.ID, resultLimit10, offset0, EMPTY, sortAscending);
 
-        // when
-        List<AuthenticatedUser> users = authenticatedUserRepository.findSearchedAuthenticatedUsers(sortKey, resultLimit, offset, searchTerm, isSortAscending);
-
-        // then
         assertThat(users.size()).isEqualTo(4);
         assertThat(users).extracting(AuthenticatedUser::getId).containsSequence(1L, 2L, 3L, 4L);
     }
 
     @Test
     public void findSearchedAuthenticatedUsers_sortedByUserIdentifier_descending() {
-        // given
-        SortKey sortKey = SortKey.USER_IDENTIFIER;
-        int resultLimit = 10;
-        int offset = 0;
-        String searchTerm = StringUtils.EMPTY;
-        boolean isSortAscending = false;
 
-        // when
-        List<AuthenticatedUser> users = authenticatedUserRepository.findSearchedAuthenticatedUsers(sortKey, resultLimit, offset, searchTerm, isSortAscending);
+        List<AuthenticatedUser> users = this.repo.findSearchedAuthenticatedUsers(
+        		SortKey.USER_IDENTIFIER, resultLimit10, offset0, EMPTY, sortDescenting);
 
-        // then
         assertThat(users.size()).isEqualTo(4);
-        assertThat(users).extracting(AuthenticatedUser::getUserIdentifier).containsSequence("superuser", "rootGroupMember", "filedownloader", "dataverseAdmin");
+        assertThat(users).extracting(AuthenticatedUser::getUserIdentifier)
+        	.containsSequence("superuser", "rootGroupMember", "filedownloader", "dataverseAdmin");
     }
 
     @Test
     public void findSearchedAuthenticatedUsers_limitedTo_2_offset_1() {
-        // given
-        SortKey sortKey = SortKey.ID;
+    	
         int resultLimit = 2;
         int offset = 1;
-        String searchTerm = StringUtils.EMPTY;
-        boolean isSortAscending = true;
 
-        // when
-        List<AuthenticatedUser> users = authenticatedUserRepository.findSearchedAuthenticatedUsers(sortKey, resultLimit, offset, searchTerm, isSortAscending);
+        List<AuthenticatedUser> users = this.repo.findSearchedAuthenticatedUsers(
+        		SortKey.ID, resultLimit, offset, EMPTY, sortAscending);
 
-        // then
         assertThat(users.size()).isEqualTo(2);
         assertThat(users).extracting(AuthenticatedUser::getId).containsSequence(2L, 3L);
     }
 
     @Test
     public void findSearchedAuthenticatedUsers_filtered() {
-        // given
-        SortKey sortKey = SortKey.ID;
-        int resultLimit = 10;
-        int offset = 0;
-        String searchTerm = "some";
-        boolean isSortAscending = true;
 
-        // when
-        List<AuthenticatedUser> users = authenticatedUserRepository.findSearchedAuthenticatedUsers(sortKey, resultLimit, offset, searchTerm, isSortAscending);
+        List<AuthenticatedUser> users = this.repo.findSearchedAuthenticatedUsers(
+        		SortKey.ID, resultLimit10, offset0, "some", sortAscending);
 
-        // then
         assertThat(users.size()).isEqualTo(2);
         assertThat(users).extracting(AuthenticatedUser::getId).containsSequence(3L, 4L);
-        assertThat(users).extracting(AuthenticatedUser::getAffiliation).containsExactly("some affiliation", "some affiliation");
+        assertThat(users).extracting(AuthenticatedUser::getAffiliation)
+        	.containsExactly("some affiliation", "some affiliation");
     }
 
     @Test
     public void findSearchedAuthenticatedUsers_filtered_noResults() {
-        // given
-        SortKey sortKey = SortKey.ID;
-        int resultLimit = 10;
-        int offset = 0;
-        String searchTerm = "XXX Non-existent";
-        boolean isSortAscending = true;
 
-        // when
-        List<AuthenticatedUser> users = authenticatedUserRepository.findSearchedAuthenticatedUsers(sortKey, resultLimit, offset, searchTerm, isSortAscending);
+        List<AuthenticatedUser> users = this.repo.findSearchedAuthenticatedUsers(
+        		SortKey.ID, resultLimit10, offset0, "Non-existent", sortAscending);
 
-        // then
-        assertThat(users.size()).isEqualTo(0);
+        assertThat(users).isEmpty();
     }
 
     @Test
     public void countSearchedAuthenticatedUsers() {
-        // given
-        String searchTerm = StringUtils.EMPTY;
 
-        // when
-        Long usersCounter = authenticatedUserRepository.countSearchedAuthenticatedUsers(searchTerm);
-
-        // then
-        assertThat(usersCounter).isEqualTo(4);
+        assertThat(this.repo.countSearchedAuthenticatedUsers(EMPTY)).isEqualTo(4);
     }
 
     @Test
     public void countSearchedAuthenticatedUsers_filtered() {
-        // given
-        String searchTerm = "lastname";
-
-        // when
-        Long usersCounter = authenticatedUserRepository.countSearchedAuthenticatedUsers(searchTerm);
-
-        // then
-        assertThat(usersCounter).isEqualTo(3);
+    	
+        assertThat(this.repo.countSearchedAuthenticatedUsers("lastname")).isEqualTo(3);
     }
 
     @Test
     public void countSearchedAuthenticatedUsers_filtered_noResults() {
-        // given
-        String searchTerm = "XXX Non-existent";
 
-        // when
-        Long usersCounter = authenticatedUserRepository.countSearchedAuthenticatedUsers(searchTerm);
-
-        // then
-        assertThat(usersCounter).isEqualTo(0);
+        assertThat(this.repo.countSearchedAuthenticatedUsers("Non-existent")).isZero();
+    }
+    
+    @Test
+    public void getSuperUserCount() {
+    	
+    	assertThat(this.repo.getSuperUserCount()).isEqualTo(2);
     }
 }
