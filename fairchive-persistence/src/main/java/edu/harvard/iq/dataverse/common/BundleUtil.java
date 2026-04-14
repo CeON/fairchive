4d25cfed78a95110c260a7a8e59f07565896552c
lbownik <lukasz.bownik@gmail.com>
2026-04-14 11:22:51 +0200

    Bundle util refactoring (#3255)
    * manual formatting, removed unused methods, performance optimizations
    
    * manual formatting, static imports
    
    * comments fixed
    
    * bugfix
    
    ---------
    
    Co-authored-by: lbownik <lbownik@icm.edu.pl>

--- fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/common/BundleUtil.java
+++ fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/common/BundleUtil.java
@@ -1,217 +1,282 @@
 package edu.harvard.iq.dataverse.common;
 
-import com.google.common.collect.Sets;
-
-import javax.faces.context.FacesContext;
-
+import static java.lang.Runtime.getRuntime;
+import static java.text.MessageFormat.format;
+import static java.util.Collections.emptyEnumeration;
+import static java.util.Locale.ENGLISH;
+import static java.util.ResourceBundle.getBundle;
+import static java.util.logging.Level.FINEST;
+import static java.util.logging.Logger.getLogger;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Paths;
-import java.text.MessageFormat;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.MissingResourceException;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Logger;
 
+import javax.faces.context.FacesContext;
+
 public class BundleUtil {
 
-    private static final Logger logger = Logger.getLogger(BundleUtil.class.getCanonicalName());
+	private static final Logger logger = getLogger(BundleUtil.class.getCanonicalName());
 
-    private static final String DEFAULT_BUNDLE_FILE = "Bundle";
+	private static final String DEFAULT_BUNDLE_FILE = "Bundle";
+	private static final String EXTENSION_SUFFIX = "extension";
+	private static final String DEFAULT_BUNDLE_FILE_EXTENTION = 
+			DEFAULT_BUNDLE_FILE + '_' + EXTENSION_SUFFIX;
 
-    private static final String EXTENSION_SUFFIX = "extension";
+	/**
+	 * This map is CRUCIAL for the performance of all methods from the class,
+	 * especially in parts for search, as the original
+	 * {@link ResourceBundle#getBundle(String)} could be very inefficient.
+	 */
+	private final static ConcurrentMap<String, ResourceBundle> bundleCache = new ConcurrentHashMap<>();
+	private final static String loaderPath = System.getProperty("dataverse.lang.directory");
+	private final static URLClassLoader externalLoader;
 
-    private static final Set<String> INTERNAL_BUNDLE_NAMES = Sets.newHashSet(
-            DEFAULT_BUNDLE_FILE, "BuiltInRoles", "MimeTypeDisplay", "MimeTypeFacets", "ValidationMessages");
+	private static final ResourceBundle EMPTY_BUNDLE = new ResourceBundle() {
+		@Override
+		protected Object handleGetObject(final String key) {
+			return null;
+		}
 
-    /**
-     * This map is CRUCIAL for the performance of all methods from
-     * the class, especially in parts for search, as the original
-     * {@link ResourceBundle#getBundle(String)} could be very
-     * inefficient.
-     */
-    private static ConcurrentMap<String, ResourceBundle> bundleCache = new ConcurrentHashMap<>();
+		@Override
+		public Enumeration<String> getKeys() {
+			return emptyEnumeration();
+		}
+	};
+	
+	static {
+		try {
+			if(loaderPath != null) {
+				externalLoader = new URLClassLoader(new URL[] { Paths.get(loaderPath).toUri().toURL() });
+				getRuntime().addShutdownHook(new Thread(() -> {
+		            try {
+		                externalLoader.close();
+		            } catch (IOException ignored) {}
+			    }));
+				
+			} else {
+				externalLoader = null;
+			}
+		} catch (final MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private static final ResourceBundle EMPTY_BUNDLE = new ResourceBundle() {
-        @Override protected Object handleGetObject(String key) { return null; }
-        @Override public Enumeration<String> getKeys() { return null; }
-    };
+	// -------------------- LOGIC --------------------
 
-    // -------------------- LOGIC --------------------
+	public static boolean hasKeyInBundle(final String key) {
+		return hasKeyInPropertyFile(key, DEFAULT_BUNDLE_FILE, getCurrentLocale());
+	}
 
-    public static boolean hasKeyInBundle(String key) {
-        return hasKeyInPropertyFile(key, DEFAULT_BUNDLE_FILE, getCurrentLocale());
-    }
+	public static String getStringFromBundle(final String key, 
+			final Object... arguments) {
+		return getStringFromBundleWithLocale(key, getCurrentLocale(), arguments);
+	}
 
-    public static String getStringFromBundle(String key, Object ... arguments) {
-        return getStringFromBundleWithLocale(key, getCurrentLocale(), arguments);
-    }
+	public static String getStringFromBundleWithLocale(final String key, 
+			final Locale locale, final Object... arguments) {
+		return getStringFromPropertyFile(DEFAULT_BUNDLE_FILE, locale, key, arguments);
+	}
 
-    public static String getStringFromBundleWithLocale(String key, Locale locale, Object... arguments) {
-        return getStringFromPropertyFile(key, DEFAULT_BUNDLE_FILE, locale)
-                .map(m -> MessageFormat.format(m, arguments))
-                .orElse(EMPTY);
-    }
+	public static boolean hasKeyInNonDefaultBundle(final String key, 
+			final String bundleName) {
+		return hasKeyInPropertyFile(key, bundleName, getCurrentLocale());
+	}
 
-    public static boolean hasKeyInNonDefaultBundle(String key, String bundleName) {
-        return hasKeyInPropertyFile(key, bundleName, getCurrentLocale());
-    }
+	public static String getStringFromNonDefaultBundle(final String key, 
+			final String bundleName, final Object... arguments) {
+		return getStringFromNonDefaultBundleWithLocale(key, bundleName, 
+				getCurrentLocale(), arguments);
+	}
 
-    public static String getStringFromNonDefaultBundle(String key, String bundleName, Object... arguments) {
-        return getStringFromNonDefaultBundleWithLocale(key, bundleName, getCurrentLocale(), arguments);
-    }
+	public static String getStringFromNonDefaultBundleWithLocale(final String key, 
+			final String bundleName, final Locale locale, final Object... arguments) {
+		return getStringFromPropertyFile(bundleName, locale, key, arguments);
+	}
 
-    public static String getStringFromNonDefaultBundleWithLocale(String key, String bundleName, Locale locale, Object... arguments) {
-        return getStringFromPropertyFile(key, bundleName, locale)
-                .map(m -> MessageFormat.format(m, arguments))
-                .orElse(EMPTY);
-    }
+	public static String getStringFromClasspathBundle(final String key, 
+			final String bundleName, final Object... arguments) {
+		return getStringFromPropertyFile(bundleName, getCurrentLocale(), key, arguments);
+	}
 
-    public static String getStringFromClasspathBundle(String key, String bundleName, Object... arguments) {
-        return getStringFromPropertyFile(key, bundleName, getCurrentLocale())
-                .map(m -> MessageFormat.format(m, arguments))
-                .orElse(EMPTY);
-    }
+	public static Locale getCurrentLocale() {
+		final FacesContext context = FacesContext.getCurrentInstance();
+		if (context == null) {
+			return ENGLISH;
+		} else if (context.getViewRoot() == null) {
+			return context.getExternalContext().getRequestLocale();
+		} else if ("en".equals(context.getViewRoot().getLocale().getLanguage())) {
+			return ENGLISH;
+		} else {
+			return context.getViewRoot().getLocale();
+		}
+	}
 
-    public static Locale getCurrentLocale() {
-        if (FacesContext.getCurrentInstance() == null) {
-            return new Locale("en");
-        } else if (FacesContext.getCurrentInstance().getViewRoot() == null) {
-            return FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
-        } else if ("en_US".equals(FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage())) {
-            return new Locale("en");
-        }
+	// -------------------- PRIVATE --------------------
 
-        return FacesContext.getCurrentInstance().getViewRoot().getLocale();
-    }
+	private static boolean hasKeyInPropertyFile(final String key, 
+			final String bundleName, final Locale locale) {
 
-    // -------------------- PRIVATE --------------------
+		if (shouldCheckForExternalBundle(bundleName) && 
+				hasKeyInExternalBundle(key, bundleName, locale)) {
+			return true;
+		}
+		ResourceBundle bundle = getCachedBundle(extendedBundleName(bundleName), locale);
+		if (isNotEmpty(bundle) && bundle.containsKey(key)) {
+			return true;
+		}
+		bundle = getCachedBundle(bundleName, locale);
+		return isNotEmpty(bundle) && bundle.containsKey(key);
+	}
 
-    private static boolean hasKeyInPropertyFile(String bundleKey, String bundleName, Locale locale) {
+	/**
+	 * Gets display name for specified bundle key. If it is external bundle, method
+	 * tries to access external directory (jvm property - dataverse.lang.directory)
+	 * where bundles are kept and return the display name.
+	 * <p>
+	 * If it is default bundle or default metadata block
+	 * #{@link DefaultMetadataBlocks#METADATA_BLOCK_NAMES} method tries to get the
+	 * name from default bundles otherwise it returns empty string.
+	 */
+	private static String getStringFromPropertyFile(final String bundleName, 
+			final Locale locale, final String key, final Object... arguments) {
+		String result = null;
 
-        if (shouldCheckForExternalBundle(bundleName) && hasKeyInExternalBundle(bundleKey, bundleName, locale)) {
-            return true;
-        }
-        ResourceBundle resourceBundle = getCachedBundle(bundleName + '_' + EXTENSION_SUFFIX, locale);
-        if (!EMPTY_BUNDLE.equals(resourceBundle) && resourceBundle.containsKey(bundleKey)) {
-            return true;
-        }
-        resourceBundle = getCachedBundle(bundleName, locale);
-        return !EMPTY_BUNDLE.equals(resourceBundle) && resourceBundle.containsKey(bundleKey);
-    }
+		if (shouldCheckForExternalBundle(bundleName)) {
+			result = getStringFromExternalBundle(key, bundleName, locale);
+			if (result != null) {
+				return maybeFormat(result, arguments);
+			}
+		}
 
-    /**
-     * Gets display name for specified bundle key. If it is external bundle,
-     * method tries to access external directory (jvm property - dataverse.lang.directory)
-     * where bundles are kept and return the display name.
-     * <p>
-     * If it is default bundle or default metadata block #{@link DefaultMetadataBlocks#METADATA_BLOCK_NAMES}
-     * method tries to get the name from default bundles otherwise it returns empty string.
-     */
-    private static Optional<String> getStringFromPropertyFile(String bundleKey, String bundleName, Locale locale) {
-        Optional<String> resolvedValue = Optional.empty();
+		result = getStringFromInternalBundle(key, extendedBundleName(bundleName), locale);
+		if (result != null) {
+			return maybeFormat(result, arguments);
+		}
+		
+		result = getStringFromInternalBundle(key, bundleName, locale);
+		if (result != null) {
+			return maybeFormat(result, arguments);
+		} else {
+			return EMPTY;
+		}
+	}
 
-        if (shouldCheckForExternalBundle(bundleName)) {
-            resolvedValue = getStringFromExternalBundle(bundleKey, bundleName, locale);
-            if (resolvedValue.isPresent()) {
-                return resolvedValue;
-            }
-        }
+	private static String getStringFromInternalBundle(final String key, 
+			final String bundleName, final Locale locale) {
 
-        resolvedValue = getStringFromInternalBundle(bundleKey, bundleName + '_' + 
-                EXTENSION_SUFFIX, locale);
-        if (resolvedValue.isPresent()) {
-            return resolvedValue;
-        }
+		final ResourceBundle bundle = getCachedBundle(bundleName, locale);
 
-        return getStringFromInternalBundle(bundleKey, bundleName, locale);
-    }
+		try {
+			return isNotEmpty(bundle)? bundle.getString(key) : null;
+		} catch (final Exception ex) {
+			if (logger.isLoggable(FINEST)) {
+				logger.finest("Could not find key \"" + key + 
+						"\" in bundle file: " + bundleName);
+			}
+			return null;
+		}
+	}
 
-    private static Optional<String> getStringFromInternalBundle(String bundleKey, String bundleName, Locale locale) {
+	private static ResourceBundle getCachedBundle(final String bundleName, 
+			final Locale locale) {
+		final String cacheKey = bundleName + '_' + locale.getLanguage();
+		ResourceBundle bundle = bundleCache.get(cacheKey);
+		if (bundle == null) {
+			try {
+				bundle = getBundle(bundleName, locale);
+			} catch (final MissingResourceException mre) {
+				bundle = EMPTY_BUNDLE;
+			}
+			bundleCache.putIfAbsent(cacheKey, bundle);
+		}
+		return bundle;
+	}
 
-        Optional<String> displayNameFromExtensionBundle = getStringFromInternalBundle(bundleKey, bundleName, EXTENSION_SUFFIX, locale);
-        return displayNameFromExtensionBundle.isPresent() ?
-                    displayNameFromExtensionBundle : getStringFromInternalBundle(bundleKey, bundleName, EMPTY, locale);
-    }
+	private static ResourceBundle getCachedExternalBundle(final String bundleName, 
+			final Locale locale) {
+		final String key = bundleName + "_ext_" + locale.getLanguage();
+		ResourceBundle bundle = bundleCache.get(key);
+		if (bundle == null) {
+			try {
+				bundle = getBundle(bundleName, locale, externalLoader);
+			} catch (final MissingResourceException ex) {
+				bundle = EMPTY_BUNDLE;
+			}
+			bundleCache.putIfAbsent(key, bundle);
+		}
+		return bundle;
+	}
 
-    private static Optional<String> getStringFromInternalBundle(String bundleKey, String bundleName, String extension, Locale locale) {
+	private static boolean shouldCheckForExternalBundle(final String bundleName) {
+		return isNotMetadataBundle(bundleName)
+				&& isNotInternal(bundleName)
+				&& loaderPath != null;
+	}
 
-        ResourceBundle resourceBundle = getCachedBundle(bundleName, locale);
+	private static boolean hasKeyInExternalBundle(final String bundleKey, 
+			final String bundleName, final Locale locale) {
+		final ResourceBundle bundle = getCachedExternalBundle(bundleName, locale);
 
-        try {
-            return !EMPTY_BUNDLE.equals(resourceBundle)
-                    ? Optional.of(resourceBundle.getString(bundleKey))
-                    : Optional.empty();
-        } catch (Exception ex) {
-            logger.finest("Could not find key \"" + bundleKey + "\" in bundle file: " + bundleName);
-            return Optional.empty();
-        }
-    }
+		return isNotEmpty(bundle) && bundle.containsKey(bundleKey);
+	}
 
-    private static ResourceBundle getCachedBundle(String bundleName, Locale locale) {
-        String cacheKey = bundleName + '_' + locale.getLanguage();
-        ResourceBundle resourceBundle = bundleCache.get(cacheKey);
-        if (resourceBundle == null) {
-            try {
-                resourceBundle = ResourceBundle.getBundle(bundleName, locale);
-            } catch (MissingResourceException mre) {
-                resourceBundle = EMPTY_BUNDLE;
-            }
-            bundleCache.putIfAbsent(cacheKey, resourceBundle);
-        }
-        return resourceBundle;
-    }
+	// IMPORTANT: this method is nearly exact copy of
+	// getStringFromInternalBundle(…), however
+	// any attempt in extracting common code from these two and pass differing parts
+	// as lambdas
+	// would cause great decrease in performance of WHOLE dataverse app.
+	private static String getStringFromExternalBundle(final String key, 
+			final String bundleName, final Locale locale) {
+		final ResourceBundle bundle = getCachedExternalBundle(bundleName, locale);
 
-    private static ResourceBundle getCachedExternalBundle(String bundleName, Locale locale) {
-        String key = bundleName + "_ext_" + locale.getLanguage();
-
-        ResourceBundle resourceBundle = bundleCache.get(key);
-        if (resourceBundle == null) {
-            try {
-                URL customBundlesDir = Paths.get(System.getProperty("dataverse.lang.directory")).toUri().toURL();
-                URLClassLoader externalBundleDirURL = new URLClassLoader(new URL[]{customBundlesDir});
-                resourceBundle = ResourceBundle.getBundle(bundleName, locale, externalBundleDirURL);
-            } catch (MalformedURLException | MissingResourceException ex) {
-                resourceBundle = EMPTY_BUNDLE;
-            }
-            bundleCache.putIfAbsent(key, resourceBundle);
-        }
-        return resourceBundle;
-    }
-
-    private static boolean shouldCheckForExternalBundle(String bundleName) {
-        return (!DefaultMetadataBlocks.METADATA_BLOCK_NAMES.contains(bundleName) && !INTERNAL_BUNDLE_NAMES.contains(bundleName))
-                && System.getProperty("dataverse.lang.directory") != null;
-    }
-
-    private static boolean hasKeyInExternalBundle(String bundleKey, String bundleName, Locale locale) {
-        ResourceBundle resourceBundle = getCachedExternalBundle(bundleName, locale);
-
-        return !EMPTY_BUNDLE.equals(resourceBundle) && resourceBundle.containsKey(bundleKey);
-    }
-
-    // IMPORTANT: this method is nearly exact copy of getStringFromInternalBundle(…), however
-    // any attempt in extracting common code from these two and pass differing parts as lambdas
-    // would cause great decrease in performance of WHOLE dataverse app.
-    private static Optional<String> getStringFromExternalBundle(String bundleKey, String bundleName, Locale locale) {
-        ResourceBundle resourceBundle = getCachedExternalBundle(bundleName, locale);
-
-        try {
-            return !EMPTY_BUNDLE.equals(resourceBundle)
-                    ? Optional.of(resourceBundle.getString(bundleKey))
-                    : Optional.empty();
-        } catch (Exception ex) {
-            logger.finest("Could not find key \"" + bundleKey + "\" in bundle file: " + bundleName);
-            return Optional.empty();
-        }
-    }
+		try {
+			return isNotEmpty(bundle) ? bundle.getString(key) : null;
+		} catch (final Exception ex) {
+			if (logger.isLoggable(FINEST)) {
+				logger.finest("Could not find key \"" + key + 
+						"\" in bundle file: " + bundleName);
+			}
+			return null;
+		}
+	}
+	
+	private static boolean isNotEmpty(final ResourceBundle bundle) {
+		return bundle != EMPTY_BUNDLE;
+	}
+	
+	private static boolean isNotInternal(final String bundleName) {
+		switch(bundleName) {
+		case DEFAULT_BUNDLE_FILE:
+		case "BuiltInRoles":
+		case "MimeTypeDisplay":
+		case "MimeTypeFacets":
+		case "ValidationMessages": return false;
+		default: return true;
+		}
+	}
+	
+	private static boolean isNotMetadataBundle(final String bundleName) {
+		return !DefaultMetadataBlocks.METADATA_BLOCK_NAMES.contains(bundleName);
+	}
+	
+	private static String maybeFormat(final String str, final Object... arguments) {
+		return arguments.length == 0 ? str : format(str, arguments);
+	}
+	
+	private static String extendedBundleName(final String bundleName) {
+		return bundleName == DEFAULT_BUNDLE_FILE
+				? DEFAULT_BUNDLE_FILE_EXTENTION
+				: bundleName + '_' + EXTENSION_SUFFIX;
+	}
 }
-
