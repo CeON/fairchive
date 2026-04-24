aec33bcc3ff6ef0a4d917e85541402697060daad
lbownik <lbownik@icm.edu.pl>
2026-04-24 09:01:14 +0200

    renamed variables, moved querries

--- fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/user/RoleAssignmentRepository.java
+++ fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/user/RoleAssignmentRepository.java
@@ -6,11 +6,10 @@ import edu.harvard.iq.dataverse.persistence.dataset.Dataset;
 import javax.ejb.Singleton;
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
-import javax.persistence.Query;
 
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.joining;
 
-import java.util.Collections;
 import java.util.List;
 
 @Singleton
@@ -24,34 +23,35 @@ public class RoleAssignmentRepository extends JpaRepository<Long, RoleAssignment
 
     // -------------------- LOGIC --------------------
 
-    public List<RoleAssignment> findByDefinitionPointId(long definitionPointId) {
+    public List<RoleAssignment> findByDefinitionPointId(final Long id) {
         return createQuery("SELECT r FROM RoleAssignment r " +
-                           "WHERE r.definitionPoint.id=:definitionPointId")
-                .setParameter("definitionPointId", definitionPointId)
+                           "WHERE r.definitionPoint.id=:id")
+                .setParameter("id", id)
                 .getResultList();
     }
 
-    public List<RoleAssignment> findByDefinitionPointIds(List<Long> definitionPointIds) {
-        return createQuery("SELECT r FROM RoleAssignment r WHERE r.definitionPoint.id IN :definitionPointIds")
-                .setParameter("definitionPointIds", definitionPointIds)
+    public List<RoleAssignment> findByDefinitionPointIds(final List<Long> ids) {
+        return createQuery("SELECT r FROM RoleAssignment r WHERE r.definitionPoint.id IN :ids")
+                .setParameter("ids", ids)
                 .getResultList();
     }
 
-    public List<RoleAssignment> findByAssigneeIdentifier(String assigneeIdentifier) {
+    public List<RoleAssignment> findByAssigneeIdentifier(final String identifier) {
         return createQuery("SELECT r FROM RoleAssignment r " +
-                           "WHERE r.assigneeIdentifier=:assigneeIdentifier")
-                .setParameter("assigneeIdentifier", assigneeIdentifier)
+                           "WHERE r.assigneeIdentifier=:identifier")
+                .setParameter("identifier", identifier)
                 .getResultList();
     }
 
-    public List<RoleAssignment> findByRoleId(long roleId) {
-        return createQuery("SELECT r FROM RoleAssignment r WHERE r.role.id=:roleId")
-                .setParameter("roleId", roleId)
+    public List<RoleAssignment> findByRoleId(final Long id) {
+        return createQuery("SELECT r FROM RoleAssignment r WHERE r.role.id=:id")
+                .setParameter("id", id)
                 .getResultList();
     }
 
-    public List<RoleAssignment> findByAssigneeIdentifiersAndDefinitionPointIds(List<String> assigneeIdentifiers,
-                                                                               List<Long> definitionPointIds) {
+    public List<RoleAssignment> findByAssigneeIdentifiersAndDefinitionPointIds(
+    		final List<String> assigneeIdentifiers,
+            final List<Long> definitionPointIds) {
         return createQuery("SELECT r FROM RoleAssignment r WHERE "
                                 + " r.assigneeIdentifier in :assigneeIdentifiers AND "
                                 + " r.definitionPoint.id in :definitionPointIds")
@@ -60,7 +60,7 @@ public class RoleAssignmentRepository extends JpaRepository<Long, RoleAssignment
                 .getResultList();
     }
 
-    public int deleteAllByAssigneeIdentifier(String identifier) {
+    public int deleteAllByAssigneeIdentifier(final String identifier) {
         return createQuery("DELETE FROM RoleAssignment r " +
                            "WHERE r.assigneeIdentifier=:assigneeIdentifier")
                 .setParameter("assigneeIdentifier", identifier)
@@ -68,32 +68,30 @@ public class RoleAssignmentRepository extends JpaRepository<Long, RoleAssignment
     }
 
     @SuppressWarnings("unchecked")
-    public List<Integer> findDataversesWithUserPermitted(List<String> identifiers) {
+    public List<Integer> findDataversesWithUserPermitted(final List<String> identifiers) {
         if (identifiers == null || identifiers.isEmpty()) {
-            return Collections.emptyList();
+            return emptyList();
+        } else {
+	        String query = "SELECT id FROM dvobject WHERE dtype = 'Dataverse' " +
+	                "and id in (select definitionpoint_id from roleassignment " +
+	                "where assigneeidentifier in ("
+	                + identifiers.stream().map(i -> "'" + i + "'").collect(joining(",")) + "));";
+	        return (List<Integer>) this.em.createNativeQuery(query).getResultList();
         }
-        String query = "SELECT id FROM dvobject WHERE dtype = 'Dataverse' " +
-                "and id in (select definitionpoint_id from roleassignment " +
-                "where assigneeidentifier in ("
-                + identifiers.stream().map(i -> "'" + i + "'").collect(joining(",")) + "));";
-        Query nativeQuery = em.createNativeQuery(query);
-        return (List<Integer>) nativeQuery.getResultList();
     }
     
     /**
      * @return A RoleAssignment or null.
      * @todo This might be a good place for Optional.
      */
-    public RoleAssignment getRoleAssignmentFromPrivateUrlToken(
-            final String privateUrlToken) {
-        if (privateUrlToken == null) {
+    public RoleAssignment getRoleAssignmentFromPrivateUrlToken(final String token) {
+        if (token == null) {
             return null;
         } else {
             try {
-                return this.em
-                        .createNamedQuery("RoleAssignment.listByPrivateUrlToken",
-                                RoleAssignment.class)
-                        .setParameter("privateUrlToken", privateUrlToken)
+                return createQuery("SELECT r FROM RoleAssignment r " + 
+                                   "WHERE r.privateUrlToken=:token")
+                        .setParameter("token", token)
                         .getSingleResult();
             } catch (final NoResultException | NonUniqueResultException ex) {
                 return null;
@@ -112,9 +110,11 @@ public class RoleAssignmentRepository extends JpaRepository<Long, RoleAssignment
             return null;
         } else {
             try {
-                return this.em.createNamedQuery(
-                        "RoleAssignment.listByAssigneeIdentifier_DefinitionPointId",
-                        RoleAssignment.class)
+                return createQuery(
+                		"SELECT r FROM RoleAssignment r " +
+                        "WHERE r.assigneeIdentifier=:assigneeIdentifier " + 
+                			"AND r.definitionPoint.id=:definitionPointId " +
+                        	"AND r.anonymized = :anonymized")
                         .setParameter("assigneeIdentifier",
                                 new PrivateUrlUser(dataset.getId()).getIdentifier())
                         .setParameter("definitionPointId", dataset.getId())
