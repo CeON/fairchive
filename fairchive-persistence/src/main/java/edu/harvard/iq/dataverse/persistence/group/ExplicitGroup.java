3c6bbc3176265ab219afbec17c23451134cbdc56
lbownik <lbownik@icm.edu.pl>
2026-05-27 18:44:42 +0200

    this, final and others

--- fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/group/ExplicitGroup.java
+++ fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/group/ExplicitGroup.java
@@ -1,17 +1,18 @@
 package edu.harvard.iq.dataverse.persistence.group;
 
-import edu.harvard.iq.dataverse.persistence.DvObject;
-import edu.harvard.iq.dataverse.persistence.user.AuthenticatedUser;
-import edu.harvard.iq.dataverse.persistence.user.RoleAssignee;
-import edu.harvard.iq.dataverse.persistence.user.RoleAssigneeDisplayInfo;
-import edu.harvard.iq.dataverse.persistence.user.User;
-import org.hibernate.validator.constraints.NotBlank;
+import static java.util.Objects.requireNonNull;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
@@ -24,11 +25,15 @@ import javax.persistence.PostLoad;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.Pattern;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
-import java.io.Serializable;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import edu.harvard.iq.dataverse.persistence.DvObject;
+import edu.harvard.iq.dataverse.persistence.JpaEntity;
+import edu.harvard.iq.dataverse.persistence.user.AuthenticatedUser;
+import edu.harvard.iq.dataverse.persistence.user.RoleAssignee;
+import edu.harvard.iq.dataverse.persistence.user.RoleAssigneeDisplayInfo;
+import edu.harvard.iq.dataverse.persistence.user.User;
 
 /**
  * A group that explicitly lists {@link RoleAssignee}s that belong to it. Implementation-wise,
@@ -69,19 +74,19 @@ import java.io.Serializable;
 @Entity
 @Table(indexes = {@Index(columnList = "owner_id"),
         @Index(columnList = "groupaliasinowner")})
-public class ExplicitGroup implements Group, Serializable {
+public class ExplicitGroup implements Group, Serializable, JpaEntity<Long> {
 
     public final static String GROUP_TYPE = "explicit";
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     Long id;
 
     /**
      * Authenticated users directly added to the group.
      */
     @ManyToMany
-    private Set<AuthenticatedUser> containedAuthenticatedUsers;
+    private Set<AuthenticatedUser> containedAuthenticatedUsers = new HashSet<>();
 
     /**
      * Explicit groups that belong to {@code this} explicit gorups.
@@ -90,7 +95,7 @@ public class ExplicitGroup implements Group, Serializable {
     @JoinTable(name = "explicitgroup_explicitgroup",
             joinColumns = @JoinColumn(name = "explicitgroup_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "containedexplicitgroups_id", referencedColumnName = "id"))
-    Set<ExplicitGroup> containedExplicitGroups;
+    Set<ExplicitGroup> containedExplicitGroups = new HashSet<>();
 
     /**
      * All the role assignees that belong to this group
@@ -100,7 +105,7 @@ public class ExplicitGroup implements Group, Serializable {
      * @see RoleAssignee#getIdentifier()
      */
     @ElementCollection
-    private Set<String> containedRoleAssignees;
+    private Set<String> containedRoleAssignees = new HashSet<>();
 
     @Column(length = 1024)
     private String description;
@@ -127,68 +132,57 @@ public class ExplicitGroup implements Group, Serializable {
     @Column(unique = true)
     private String groupAlias;
 
-    public ExplicitGroup() {
-        containedAuthenticatedUsers = new HashSet<>();
-        containedExplicitGroups = new HashSet<>();
-        containedRoleAssignees = new TreeSet<>();
-    }
-
     public Set<AuthenticatedUser> getContainedAuthenticatedUsers() {
-        return containedAuthenticatedUsers;
+        return this.containedAuthenticatedUsers;
     }
 
     public Set<ExplicitGroup> getContainedExplicitGroups() {
-        return containedExplicitGroups;
+        return this.containedExplicitGroups;
     }
 
-    public void add(User u) {
-        if (u == null) {
-            throw new IllegalArgumentException("Cannot add a null user to an explicit group.");
-        }
-        if (u instanceof AuthenticatedUser) {
-            containedAuthenticatedUsers.add((AuthenticatedUser) u);
+    public void add(final User user) {
+    	requireNonNull(user, "Cannot add a null user to an explicit group.");
+        if (user instanceof AuthenticatedUser) {
+            this.containedAuthenticatedUsers.add((AuthenticatedUser) user);
         } else {
-            containedRoleAssignees.add(u.getIdentifier());
+            this.containedRoleAssignees.add(user.getIdentifier());
         }
     }
 
     /**
      * Adds the {@link RoleAssignee} to {@code this} group.
      *
-     * @param ra the role assignee to be added to this group.
+     * @param assignee the role assignee to be added to this group.
      * @throws GroupException if {@code ra} is a group, and is either an ancestor of {@code this},
      *                        or is defined in a dataverse that is not an ancestor of {@code this.owner}.
      */
-    public void add(RoleAssignee ra)  {
-
-        if (ra.equals(this)) {
+    public void add(final RoleAssignee assignee)  {
+        if (assignee.equals(this)) {
             throw new GroupException(this, "A group cannot be added to itself.");
         }
-
-        if (ra instanceof User) {
-            add((User) ra);
-
+        if (assignee instanceof User) {
+            add((User) assignee);
         } else {
-            if (ra instanceof ExplicitGroup) {
+            if (assignee instanceof ExplicitGroup) {
                 // validate no circular deps
-                ExplicitGroup g = (ExplicitGroup) ra;
-                if (g.structuralContains(this)) {
+                final ExplicitGroup group = (ExplicitGroup) assignee;
+                if (group.structuralContains(this)) {
                     throw new GroupException(this, "A group cannot be added to one of its childs.");
                 }
-                if (g.owner.isAncestorOf(owner)) {
-                    containedExplicitGroups.add(g);
+                if (group.owner.isAncestorOf(this.owner)) {
+                    this.containedExplicitGroups.add(group);
                 } else {
-                    throw new GroupException(this, "Cannot add " + g + ", as it is not defined in " + owner + " or one of its ancestors.");
+                    throw new GroupException(this, "Cannot add " + group + 
+                    		", as it is not defined in " + this.owner + 
+                    		" or one of its ancestors.");
                 }
             } else {
-                containedRoleAssignees.add(ra.getIdentifier());
+                this.containedRoleAssignees.add(assignee.getIdentifier());
             }
-
         }
-
     }
 
-    public void remove(RoleAssignee roleAssignee) {
+    public void remove(final RoleAssignee roleAssignee) {
         removeByRoleAssgineeIdentifier(roleAssignee.getIdentifier());
     }
 
@@ -200,31 +194,30 @@ public class ExplicitGroup implements Group, Serializable {
      * @return A list of the role assignee identifiers.
      */
     public Set<String> getContainedRoleAssgineeIdentifiers() {
-        Set<String> retVal = new TreeSet<>();
-        retVal.addAll(containedRoleAssignees);
-        for (ExplicitGroup subg : getContainedExplicitGroups()) {
-            retVal.add(subg.getIdentifier());
+        final Set<String> result = new TreeSet<>();
+        result.addAll(this.containedRoleAssignees);
+        for (final ExplicitGroup subg : getContainedExplicitGroups()) {
+            result.add(subg.getIdentifier());
         }
-        for (AuthenticatedUser au : containedAuthenticatedUsers) {
-            retVal.add(au.getIdentifier());
+        for (final AuthenticatedUser au : this.containedAuthenticatedUsers) {
+            result.add(au.getIdentifier());
         }
-
-        return retVal;
+        return result;
     }
 
-    public void removeByRoleAssgineeIdentifier(String idtf) {
-        if (containedRoleAssignees.contains(idtf)) {
-            containedRoleAssignees.remove(idtf);
+    public void removeByRoleAssgineeIdentifier(final String identifier) {
+        if (this.containedRoleAssignees.contains(identifier)) {
+            this.containedRoleAssignees.remove(identifier);
         } else {
-            for (AuthenticatedUser au : containedAuthenticatedUsers) {
-                if (au.getIdentifier().equals(idtf)) {
-                    containedAuthenticatedUsers.remove(au);
+            for (final AuthenticatedUser au : this.containedAuthenticatedUsers) {
+                if (au.getIdentifier().equals(identifier)) {
+                    this.containedAuthenticatedUsers.remove(au);
                     return;
                 }
             }
-            for (ExplicitGroup eg : containedExplicitGroups) {
-                if (eg.getIdentifier().equals(idtf)) {
-                    containedExplicitGroups.remove(eg);
+            for (final ExplicitGroup eg : this.containedExplicitGroups) {
+                if (eg.getIdentifier().equals(identifier)) {
+                    this.containedExplicitGroups.remove(eg);
                     return;
                 }
             }
@@ -233,10 +226,10 @@ public class ExplicitGroup implements Group, Serializable {
 
     @Override
     public String getDescription() {
-        return description;
+        return this.description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(final String description) {
         this.description = description;
     }
 
@@ -247,36 +240,24 @@ public class ExplicitGroup implements Group, Serializable {
      * a specific {@link AuthenticatedUser} {@code u}, {@code structuralContains(u)}
      * would return {@code false} while {@code contains( request(u, ...) )} would return true;
      *
-     * @param ra
+     * @param assignee
      * @return {@code true} iff the role assignee is structurally a part of the group.
      */
-    public boolean structuralContains(RoleAssignee ra) {
+    public boolean structuralContains(final RoleAssignee assignee) {
         // direct containment
-        if (ra instanceof AuthenticatedUser) {
-            if (containedAuthenticatedUsers.contains(ra)) {
-                return true;
-            }
-
-        } else if (ra instanceof ExplicitGroup) {
-            if (containedExplicitGroups.contains(ra)) {
-                return true;
-            }
-
+        if (assignee instanceof AuthenticatedUser &&
+        		this.containedAuthenticatedUsers.contains(assignee)) {
+        	return true;
+        } else if (assignee instanceof ExplicitGroup &&
+        		this.containedExplicitGroups.contains(assignee)) {
+        	return true;
+        } else if (this.containedRoleAssignees.contains(assignee.getIdentifier())) {
+            return true;
         } else {
-            if (containedRoleAssignees.contains(ra.getIdentifier())) {
-                return true;
-            }
+        	// no direct containment. Recurse.
+        	return this.containedExplicitGroups.stream().
+        			anyMatch(group -> group.structuralContains(assignee));
         }
-
-        // no direct containment. Recurse.
-        for (ExplicitGroup eg : containedExplicitGroups) {
-            if (eg.structuralContains(ra)) {
-                return true;
-            }
-        }
-
-        return false;
-
     }
 
     /**
@@ -316,49 +297,46 @@ public class ExplicitGroup implements Group, Serializable {
     }
 
     public String getGroupAliasInOwner() {
-        return groupAliasInOwner;
+        return this.groupAliasInOwner;
     }
 
-    public void setGroupAliasInOwner(String groupAliasInOwner) {
+    public void setGroupAliasInOwner(final String groupAliasInOwner) {
         this.groupAliasInOwner = groupAliasInOwner;
     }
 
     @Override
     public String getAlias() {
-        return groupAlias;
+        return this.groupAlias;
     }
 
     @Override
     public String getDisplayName() {
-        return displayName;
+        return this.displayName;
     }
 
-    public void setDisplayName(String displayName) {
+    public void setDisplayName(final String displayName) {
         this.displayName = displayName;
     }
 
     public DvObject getOwner() {
-        return owner;
+        return this.owner;
     }
 
-    public void setOwner(DvObject owner) {
+    public void setOwner(final DvObject owner) {
         this.owner = owner;
     }
 
     public Long getId() {
-        return id;
+        return this.id;
     }
 
-    public void setId(Long id) {
+    public void setId(final Long id) {
         this.id = id;
     }
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.id);
-        hash = 53 * hash + Objects.hashCode(this.groupAliasInOwner);
-        return hash;
+        return 53 * Objects.hashCode(this.id) + Objects.hashCode(this.groupAliasInOwner);
     }
 
     @Override
@@ -370,8 +348,8 @@ public class ExplicitGroup implements Group, Serializable {
             return false;
         }
         final ExplicitGroup other = (ExplicitGroup) obj;
-        if (id != null && other.getId() != null) {
-            return Objects.equals(id, other.getId());
+        if (this.id != null && other.getId() != null) {
+            return Objects.equals(this.id, other.getId());
         } else {
             return Objects.equals(this.groupAliasInOwner, other.groupAliasInOwner)
                     && Objects.equals(this.owner, other.owner);
@@ -386,7 +364,7 @@ public class ExplicitGroup implements Group, Serializable {
      * @return the strings of the role assignees in this group.
      */
     public Set<String> getContainedRoleAssignees() {
-        return containedRoleAssignees;
+        return this.containedRoleAssignees;
     }
 
     @Override
