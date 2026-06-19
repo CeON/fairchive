dfd1fc46c4cb033769ccc0e5b9b5f0652c98a28b
lbownik <lbownik@icm.edu.pl>
2026-06-19 14:54:05 +0200

    wip

--- fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/api/imports/ImportGenericServiceBeanIT.java
+++ fairchive-webapp/src/test/java/edu/harvard/iq/dataverse/api/imports/ImportGenericServiceBeanIT.java
@@ -1,29 +1,33 @@
 package edu.harvard.iq.dataverse.api.imports;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import edu.harvard.iq.dataverse.DatasetFieldServiceBean;
-import edu.harvard.iq.dataverse.api.dto.DatasetDTO;
-import edu.harvard.iq.dataverse.arquillian.arquillianexamples.WebappArquillianDeployment;
-import edu.harvard.iq.dataverse.persistence.dataset.Dataset;
-import edu.harvard.iq.dataverse.persistence.dataset.MetadataBlockRepository;
-import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
-import edu.harvard.iq.dataverse.util.json.JsonParseException;
-import edu.harvard.iq.dataverse.util.json.JsonParser;
-import org.apache.commons.io.IOUtils;
-import org.assertj.core.api.Assertions;
-import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
-import org.jboss.arquillian.transaction.api.annotation.Transactional;
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.xml.stream.XMLStreamException;
-import java.io.IOException;
-import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import edu.harvard.iq.dataverse.DatasetFieldServiceBean;
+import edu.harvard.iq.dataverse.api.dto.DatasetDTO;
+import edu.harvard.iq.dataverse.arquillian.arquillianexamples.WebappArquillianDeployment;
+import edu.harvard.iq.dataverse.persistence.dataset.Dataset;
+import edu.harvard.iq.dataverse.persistence.dataset.DatasetField;
+import edu.harvard.iq.dataverse.persistence.dataset.MetadataBlockRepository;
+import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
+import edu.harvard.iq.dataverse.util.json.JsonParseException;
+import edu.harvard.iq.dataverse.util.json.JsonParser;
 
 @Transactional(TransactionMode.ROLLBACK)
 public class ImportGenericServiceBeanIT extends WebappArquillianDeployment {
@@ -51,7 +55,27 @@ public class ImportGenericServiceBeanIT extends WebappArquillianDeployment {
         final DatasetDTO datasetDTO = importGenericServiceBean.processOAIDCxml(xml);
 
         //then
-        Assertions.assertThat(parseDTOtoEntity(datasetDTO)).isNotNull();
+        //Assertions.assertThat(parseDTOtoEntity(datasetDTO)).isNotNull();
+        
+        Dataset dataset = parseDTOtoEntity(datasetDTO);
+        
+        System.out.println("==============================================");
+        System.out.println("GlogalbId: " + dataset.getGlobalId());
+        System.out.println("==============================================");
+        
+        List<DatasetField> fields = dataset.getLatestVersion().getDatasetFields();
+        
+        fields.forEach(field -> {
+        	System.out.println(field.getTypeName() + " -> " + field.getValue());
+        	
+        	field.getChildren().forEach(child -> {
+        		System.out.println("> " + child.getTypeName() + " -> " + child.getValue());
+        	});
+        	
+        });
+        
+        System.out.println("==============================================");
+        
     }
 
     private Dataset parseDTOtoEntity(DatasetDTO datasetDTO) throws JsonParseException {
