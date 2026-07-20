c0d561916c59a2248b29e26444e92964245920f8
lbownik <lbownik@icm.edu.pl>
2026-07-20 14:10:06 +0200

    moved named query to repository class, final this, variables renamed

--- fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/group/SamlGroup.java
+++ fairchive-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/group/SamlGroup.java
@@ -1,19 +1,17 @@
 package edu.harvard.iq.dataverse.persistence.group;
 
-import edu.harvard.iq.dataverse.persistence.JpaEntity;
-import edu.harvard.iq.dataverse.persistence.user.RoleAssigneeDisplayInfo;
+import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-import java.io.Serializable;
-import java.util.Objects;
+import edu.harvard.iq.dataverse.persistence.JpaEntity;
+import edu.harvard.iq.dataverse.persistence.user.RoleAssigneeDisplayInfo;
 
 /**
  * Persistence for Saml groups.
@@ -21,9 +19,6 @@ import java.util.Objects;
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "samlgroup")
-@NamedQueries({
-    @NamedQuery(name = "SamlGroup.findByEntityId", query = "SELECT g FROM SamlGroup g WHERE g.entityId = :entityId")
-})
 public class SamlGroup implements JpaEntity<Long>, Group, Serializable {
 
     public final static String GROUP_TYPE = "saml";
@@ -48,7 +43,7 @@ public class SamlGroup implements JpaEntity<Long>, Group, Serializable {
 
     public SamlGroup() { }
 
-    public SamlGroup(String name, String entityId) {
+    public SamlGroup(final String name, final String entityId) {
         this.name = name;
         this.entityId = entityId;
     }
@@ -61,15 +56,15 @@ public class SamlGroup implements JpaEntity<Long>, Group, Serializable {
     // -------------------- GETTERS --------------------
 
     public Long getId() {
-        return id;
+        return this.id;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getEntityId() {
-        return entityId;
+        return this.entityId;
     }
 
     @Override
@@ -108,21 +103,21 @@ public class SamlGroup implements JpaEntity<Long>, Group, Serializable {
     // -------------------- equals & hashCode --------------------
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
+    public boolean equals(final Object other) {
+        if (this == other) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (other == null || getClass() != other.getClass()) {
             return false;
         }
-        SamlGroup samlGroup = (SamlGroup) o;
-        return Objects.equals(id, samlGroup.id)
-                && name.equals(samlGroup.name)
-                && entityId.equals(samlGroup.entityId);
+        final SamlGroup samlGroup = (SamlGroup) other;
+        return Objects.equals(this.id, samlGroup.id)
+                && this.name.equals(samlGroup.name)
+                && this.entityId.equals(samlGroup.entityId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, entityId);
+        return Objects.hash(this.id, this.name, this.entityId);
     }
 }
