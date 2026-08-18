e85b9d3598284d9a97e8f514304cb9a8bb7c40e4
tomaszblazejczyk <blazejc@icm.edu.pl>
2026-08-18 10:28:50 +0200

    Closes #3351: On banner page, "Reuse" does not reuse anything

--- fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/bannersandmessages/banners/NewBannerPage.java
+++ fairchive-webapp/src/main/java/edu/harvard/iq/dataverse/bannersandmessages/banners/NewBannerPage.java
@@ -42,6 +42,7 @@ public class NewBannerPage implements Serializable {
     private Long dataverseId;
     private Dataverse dataverse;
     private final DataverseBanner banner = new DataverseBanner();
+	private Long bannerTemplateId;
 
     @Inject
     public NewBannerPage(final PermissionsWrapper permissionsWrapper,
@@ -95,6 +96,25 @@ public class NewBannerPage implements Serializable {
     public void setDataverseId(final Long dataverseId) {
         this.dataverseId = dataverseId;
         this.dataverse = this.dataverseRepo.findById(this.dataverseId).orElse(null);
+    }
+    
+    public Long getBannerTemplateId() {
+		return bannerTemplateId;
+	}
+
+    public void setBannerTemplateId(Long bannerTemplateId) {
+        this.bannerTemplateId = bannerTemplateId;
+    	if (bannerTemplateId != null) {
+        	bannerRepo.findById(bannerTemplateId).ifPresent(template -> {
+        		this.banner.getLocalizedBanners().forEach(localizedBanner -> {
+        			template.getBannerFor(localizedBanner.getLocale()).ifPresent(templateLocalizedBanner -> {
+        				localizedBanner.setContentType(templateLocalizedBanner.getContentType());
+        				localizedBanner.setImage(templateLocalizedBanner.getImage());
+        				localizedBanner.setImageLink(templateLocalizedBanner.getImageLink());
+        			});
+        		});
+        	});
+        }
     }
 
     public Dataverse getDataverse() {
